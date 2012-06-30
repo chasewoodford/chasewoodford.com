95097de7874d7ea436c58c8bfaa5b6b54e3aa8e9
Chase Woodford <chase@chasewoodford.com>
2012-06-29 21:35:09 -0400

    edit to db name

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'blog');
+define('DB_NAME', 'db421787423');
 
 /** MySQL database username */
 define('DB_USER', 'dbo421787423');
