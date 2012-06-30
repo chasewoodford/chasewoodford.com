d85fd163263214268d0da0a9b38ec79b0a49529a
Chase Woodford <chase@chasewoodford.com>
2012-06-29 21:44:58 -0400

    show me the posts!

--- blog/index.php
+++ blog/index.php
@@ -17,7 +17,12 @@ define('WP_USE_THEMES', false);
 require('./wp-blog-header.php');
 ?>
 
-<?php query_posts('showposts=3'); ?>
-<?php while (have_posts()) : the_post(); ?>
-<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a><br />
-<?php endwhile;?>
+<?php
+$posts = get_posts('numberposts=10&order=ASC&orderby=post_title');
+foreach ($posts as $post) : start_wp(); ?>
+<?php the_date(); echo "<br />"; ?>
+<?php the_title(); ?>
+<?php the_excerpt(); ?>
+<?php
+endforeach;
+?>
