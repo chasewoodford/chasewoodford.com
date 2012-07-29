2c6aa9aa534206ae21107b6b8c8fa7168d243e3e
Chase Woodford <chase(at)chasewoodford(dot)com>
2012-07-28 21:23:10 -0400

    wordpress test

--- index.php
+++ index.php
@@ -42,14 +42,14 @@ require('./wp-blog-header.php');
     <?php while (have_posts()) : the_post(); ?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <div class="stumblr-title"><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></div>
-            <div class="stumblr-content">
-                <?php if ( has_post_thumbnail() ) { ?><div class="stumblr-image"><?php the_post_thumbnail( 'stumblr-large-image' );  ?></div><?php } ?>
+            <div><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></div>
+            <div>
+                <?php if ( has_post_thumbnail() ) { ?><div><?php the_post_thumbnail( 'stumblr-large-image' );  ?></div><?php } ?>
                 <?php the_content(''); ?>
             </div>
 
-            <div class="stumblr-meta">
-                <p><span class="stumblr-date"><?php the_time(get_option('date_format')); ?></span> <span class="stumblr-category"> <?php the_category(', ') ?></span></p>
+            <div>
+                <p><span><?php the_time(get_option('date_format')); ?></span> <span> <?php the_category(', ') ?></span></p>
             </div>
             <div class="clear"></div></div><!-- end post -->
 
