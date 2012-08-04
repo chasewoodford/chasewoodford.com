30b98c22cedab53a3648473f269674373f0977e7
Chase Woodford <chase(at)chasewoodford(dot)com>
2012-08-03 21:32:31 -0400

    wp test

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
 ?>
 
 <?php
-$posts = get_posts('numberposts=10&order=ASC&orderby=post_title');
+$posts = get_posts('numberposts=10&order=DSC&orderby=post_title');
 foreach ($posts as $post) : start_wp(); ?>
-    <?php the_date(); echo "<br />"; ?>
     <?php the_title(); ?>
     <?php the_excerpt(); ?>
+    <?php the_date(); echo "<br />"; ?>
     <?php
 endforeach;
 ?>
