f60b68a397be2be129b0289559fefd1c5352ac33
Chase Woodford <chase(at)chasewoodford(dot)com>
2012-08-04 20:15:08 -0400

    wp test

--- wp-content/themes/stumblr/header.php
+++ wp-content/themes/stumblr/header.php
@@ -1,17 +1,7 @@
 <?php  $options = get_option('plugin_options');
     $stumblr_color_scheme = $options['stumblr_color_scheme'];
-    $stumblr_logo = $options['stumblr_logo'];
-    $stumblr_show_title = $options['stumblr_show_title'];
-    $stumblr_highlight_color = $options['stumblr_highlight_color'];
 ?>			
 
-<?php if ($stumblr_highlight_color != '') { ?><style type="text/css"> a:hover { color:<?php echo $stumblr_highlight_color; ?>; }  .side-widget ul li a:hover { color:<?php echo $stumblr_highlight_color; ?>; }   .footer-widget ul li a:hover { color:<?php echo $stumblr_highlight_color; ?>; }  </style><?php  } ?>
-
- 
-    
-
-
-	
 <div id="wrap">
 	<div id="stumblr-sidebar">
         <?php if ( is_active_sidebar( 'stumblr_widgets')) { ?>
