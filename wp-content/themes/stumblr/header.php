4144d9efba0ef5c66f4335682e28668e66bbb5b6
Chase Woodford <chase(at)chasewoodford(dot)com>
2012-08-04 20:02:38 -0400

    overwriting wp theme css

--- wp-content/themes/stumblr/header.php
+++ wp-content/themes/stumblr/header.php
@@ -1,19 +1,3 @@
-<!--<!DOCTYPE html>-->
-<!-- Stumblr WordPress Theme by Eleven Themes (http://www.eleventhemes.com) - Proudly powered by WordPress (http://wordpress.org) -->
-
-
-
-<!-- meta -->
-<!--<html --><?php //language_attributes();?><!--> -->
-<!--<meta charset="--><?php //bloginfo('charset'); ?><!--" />-->
-<!--<title>--><?php //bloginfo('sitename'); ?><!-- --><?php //wp_title(); ?><!--</title>-->
-<!---->
-<!--<link rel="pingback" href="--><?php //bloginfo('pingback_url'); ?><!--" />-->
-
-<!-- styles -->
-<!--<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>-->
-<!--<link rel="stylesheet" type="text/css" media="all" href="--><?php //echo get_template_directory_uri(); ?><!--/css/reset.css" />-->
-
 <?php  $options = get_option('plugin_options');
     $stumblr_color_scheme = $options['stumblr_color_scheme'];
     $stumblr_logo = $options['stumblr_logo'];
@@ -21,16 +5,6 @@
     $stumblr_highlight_color = $options['stumblr_highlight_color'];
 ?>			
 
-<?php //if ($stumblr_color_scheme == 'custom') { ?>
-<!--	<link rel="stylesheet" href="--><?php //echo get_template_directory_uri(); ?><!--/css/custom.css"/>-->
-<?php // } elseif ($stumblr_color_scheme == 'minimal') {  ?>
-<!--<link rel="stylesheet" href="--><?php //echo get_template_directory_uri(); ?><!--/css/minimal.css"/>-->
-<?php // } else {?>
-<!--<link rel="stylesheet" href="--><?php //echo get_template_directory_uri(); ?><!--/css/light.css"/>-->
-<?php // } ?>
-
-
-<?php if ($stumblr_show_title == 'no') { ?><style type="text/css"> .stumblr-title { display:none;}</style><?php  } ?>
 <?php if ($stumblr_highlight_color != '') { ?><style type="text/css"> a:hover { color:<?php echo $stumblr_highlight_color; ?>; }  .side-widget ul li a:hover { color:<?php echo $stumblr_highlight_color; ?>; }   .footer-widget ul li a:hover { color:<?php echo $stumblr_highlight_color; ?>; }  </style><?php  } ?>
 
  
