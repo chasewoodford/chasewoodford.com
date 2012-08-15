3294cf7eb2914c94fa11a7686dd5f2978f420586
cwoodford <chase@chasewoodford.com>
2012-08-14 21:34:45 -0400

    wp test

--- blog/index.php
+++ blog/index.php
@@ -1,71 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Chase Woodford &#124; Designer &amp; Developer</title>
-    <meta name="description" content="">
-    <meta name="author" content="Chase Woodford">
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/compiled/home.css">
-</head>
-<body id="top">
-<?php
-define('WP_USE_THEMES', true);
-require('./wp-blog-header.php');
-?>
 <h1>chasewoodford.com</h1>
 
-
-<!--<div id="contact">-->
-<!--<div class="content">-->
-<!--<p>-->
-<!--Shoot me a note! <a class="inline" href="#top" onclick="leavingContact();">Click here to go back</a>.-->
-<!--</p>-->
-<!--<div id="form">-->
-<!--<form id="ajax-contact-form" action="javascript:alert('success!');">-->
-<!--<fieldset class="info_fieldset">-->
-<!--<div id="note"></div>-->
-<!--<div id="fields">-->
-<!--<label for="name">Name</label>-->
-<!--<input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" /><br />-->
-<!--<label for="email">Email</label>-->
-<!--<input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />-->
-<!--<label for="comments">Comments</label>-->
-<!--<textarea id="comments" class="textbox textbox2" tabindex="3" name="message" rows="10" cols="50" spellcheck="true"></textarea>-->
-<!--<input class="formSend btn btn-large btn-primary" type="submit" tabindex="4" value="Send Message" />-->
-<!--<input class="formClear btn btn-large btn-danger" type="reset" tabindex="5" value="Clear" />-->
-<!--</div>-->
-<!--</fieldset>-->
-<!--</form>-->
-<!--</div>-->
-<!--</div>-->
-<!--</div>&lt;!&ndash; end contact &ndash;&gt;-->
-<!--<footer id="footer">-->
-<!--<div id="quickLinks">-->
-<!--<p>-->
-<!--<a href="#" onClick="quickLinks();">Quick links &#187;</a>-->
-<!--</p>-->
-<!--</div>-->
-<!--<div id="copyright" role="contentinfo">-->
-<!--<p>-->
-<!--&copy; 2012 chasewoodford.com-->
-<!--</p>-->
-<!--</div>-->
-<!--<div id="belowFooter">-->
-<!--<ul>-->
-<!--<li><a href="https://twitter.com/#!/chase1263070" title="Twitter"><i class="social">t</i></a></li>-->
-<!--<li><a href="http://www.linkedin.com/in/chasewoodford" title="LinkedIn"><i class="social">i</i></a></li>-->
-<!--<li><a href="http://www.last.fm/music/Chase+Woodford/Long+Story+Short" title="last.fm"><i class="social">6</i></a></li>-->
-<!--<li><a href="http://pinterest.com/chasewoodford/" title="Pinterest"><i class="social">&</i></a></li>-->
-<!--<li><a href="#" title="Download resume"><i class="social">c</i></a></li>-->
-<!--<li><a href="#contact" title="Contact me"><i class="social">?</i></a></li>-->
-<!--</ul>-->
-<!--</div>-->
-<!--</footer>&lt;!&ndash; end footer &ndash;&gt;-->
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="javascript/jquery-1.7.min.js"><\/script>')</script>
-<script src="javascript/contact_form.js"></script>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <script type="text/javascript">
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-28396921-1']);
