af965d585f448eca4c01d75ab1a930b55ed311f1
cwoodford <chase@chasewoodford.com>
2012-08-14 23:05:11 -0400

    wp test

--- blog/index.php
+++ blog/index.php
@@ -1,34 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Chase Woodford &#124; Designer &amp; Developer</title>
-    <meta name="description" content="">
-    <meta name="author" content="Chase Woodford">
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="../css/compiled/home.css">
-</head>
-<body id="top">
+
 <?php
 define('WP_USE_THEMES', true);
 require('./wp-blog-header.php');
 ?>
-<h1>chasewoodford.com</h1>
-
-</body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="javascript/jquery-1.7.min.js"><\/script>')</script>
-<script src="javascript/contact_form.js"></script>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-28396921-1']);
-    _gaq.push(['_setDomainName', 'chasewoodford.com']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-</script>
-</html>
