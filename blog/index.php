a87707488e6932b06c06dfca74480495f4a70e69
cwoodford <chase@chasewoodford.com>
2012-08-14 22:04:57 -0400

    wp test

--- blog/index.php
+++ blog/index.php
@@ -1,4 +1,8 @@
 <?php
-define('WP_USE_THEMES', true);
 require('../wp-blog-header.php');
 ?>
+
+<?php query_posts('showposts=3'); ?>
+<?php while (have_posts()) : the_post(); ?>
+<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a><br />
+<?php endwhile;?>
