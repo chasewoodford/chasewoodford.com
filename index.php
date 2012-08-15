b658fcb48a94aeac2e374341ebc6e142071242e2
cwoodford <chase@chasewoodford.com>
2012-08-15 00:21:24 -0400

    wp test

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ require('./blog/wp-blog-header.php');
 ?>
 
 <?php
-$posts = get_posts('numberposts=1&order=ASC&orderby=post_title');
+$posts = get_posts('numberposts=1&order=DSC&orderby=post_title');
 foreach ($posts as $post) : start_wp(); ?>
     <?php the_date(); echo "<br />"; ?>
     <?php the_title(); ?>
