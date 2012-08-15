a001c6a9f1e97aa78e700cf2ab9c90222d2418f0
cwoodford <chase@chasewoodford.com>
2012-08-15 00:16:54 -0400

    wp test

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a>
 </p>
 <?php
-define('WP_USE_THEMES', false);
+define('WP_USE_THEMES', true);
 require('./blog/wp-blog-header.php');
 ?>
 
