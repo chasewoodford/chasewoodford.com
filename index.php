29e6f565715e82d5ad5ff32eb110cb3d4398b923
cwoodford <chase@chasewoodford.com>
2012-08-15 00:18:26 -0400

    wp test

--- index.php
+++ index.php
@@ -20,9 +20,19 @@
 <p class="parking">
     <a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a>
 </p>
+
 <?php
-define('WP_USE_THEMES', true);
 require('./blog/wp-blog-header.php');
+?>
+
+<?php
+$posts = get_posts('numberposts=10&order=ASC&orderby=post_title');
+foreach ($posts as $post) : start_wp(); ?>
+    <?php the_date(); echo "<br />"; ?>
+    <?php the_title(); ?>
+    <?php the_excerpt(); ?>
+    <?php
+endforeach;
 ?>
 
 <!--<div id="contact">-->
