37a5ce643a6628ba60d77850e71528a6e65fe736
cwoodford <chase@chasewoodford.com>
2012-08-14 21:36:08 -0400

    wp test

--- blog/index.php
+++ blog/index.php
@@ -1,18 +1,1 @@
-<h1>chasewoodford.com</h1>
-
-</body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="javascript/jquery-1.7.min.js"><\/script>')</script>
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-28396921-1']);
-    _gaq.push(['_setDomainName', 'chasewoodford.com']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-</script>
-</html>
+<p>test!</p>
