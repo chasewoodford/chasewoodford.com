46f953b2d6b2aa190bccc7b04efe728273864e0f
cwoodford <chase@chasewoodford.com>
2012-08-14 21:57:24 -0400

    wp test

--- blog/index.php
+++ blog/index.php
@@ -1,23 +1,4 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', false);
-
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
-?>
-
-<?php
 $posts = get_posts('numberposts=10&order=DSC&orderby=post_title');
 foreach ($posts as $post) : start_wp(); ?>
 <?php the_date(); echo "<br />"; ?>
