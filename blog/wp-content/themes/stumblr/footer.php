0124b6a5135676539f2f72dc9e1055481c2f401b
cwoodford <chase@chasewoodford.com>
2012-08-16 21:17:03 -0400

    footer update

--- blog/wp-content/themes/stumblr/footer.php
+++ blog/wp-content/themes/stumblr/footer.php
@@ -7,18 +7,15 @@
 <?php }  ?>     
       
 
-
- <div id="copyright" role="contentinfo">
-    <p>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?>&nbsp;&#8226;&nbsp;RSS</p>
- </div>
- <div>
-     <a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a>
- </div>
+<footer>
+     <div id="copyright" role="contentinfo">
+        <p>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?>&nbsp;&#8226;&nbsp;RSS</p>
+     </div>
+     <div>
+         <a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a>
+     </div>
+</footer>
      
-</div><!-- // wrap -->   
-
-	<?php wp_footer(); ?>
-	
+</div>
 </body>
-
 </html>
