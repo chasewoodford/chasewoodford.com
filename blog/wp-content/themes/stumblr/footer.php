4798cace6a97a5702b9538094109c269060ed818
cwoodford <chase@chasewoodford.com>
2012-08-16 21:12:41 -0400

    add twitter widget

--- blog/wp-content/themes/stumblr/footer.php
+++ blog/wp-content/themes/stumblr/footer.php
@@ -10,11 +10,15 @@
 
  <div id="copyright">
     <p>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?>&nbsp;&#8226;&nbsp;RSS</p>
- </div><!-- // copyright -->   
+ </div>
+ <div>
+     <a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a>
+ </div>
      
 </div><!-- // wrap -->   
 
 	<?php wp_footer(); ?>
 	
 </body>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </html>
