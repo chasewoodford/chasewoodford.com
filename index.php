ff62c9150a7400402a5c17621d4bde22333fff8d
cwoodford <chase@chasewoodford.com>
2012-08-18 14:58:39 -0400

    added primary nav and search

--- index.php
+++ index.php
@@ -11,33 +11,33 @@
 <body id="top">
 <div class="wrapper">
     <header class="grid-10">
-        <div class="pulse inline">
-            <img class="logo" src="images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
-        </div>
-        <h1 class="inline sitename">chasewoodford.com</h1>
-        <nav class="nav-primary inline grid-10">
+        <img class="logo pulse" src="images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
+        <h1 class="sitename">chasewoodford.com</h1>
+        <aside class="search-box">
+            <input type="text"/>
+            <input type="submit"/>
+        </aside>
+        <nav class="nav-primary">
             <ul>
                 <li>
-                    <a href="#"><h2>about</h2></a>
+                    <a class="nav-primary-link" href="#"><h2>about</h2></a>
                 </li>
+                <li><h2>&bull;</h2></li>
                 <li>
                     <a href="#"><h2>work</h2></a>
                 </li>
+                <li><h2>&bull;</h2></li>
                 <li>
                     <a href="#"><h2>blog</h2></a>
                 </li>
+                <li><h2>&bull;</h2></li>
                 <li>
                     <a href="#"><h2>contact</h2></a>
                 </li>
             </ul>
         </nav>
-    </header>
-    <section>
-        <p class="grid-10">
-            This site was taken down because <&#37;=reason&#37;>. It will be back up <&#37;=deadline&#37;>.
-        </p>
-    </section>
 
+    </header>
 
     <?php
     require('./blog/wp-blog-header.php');
@@ -52,37 +52,6 @@
         <?php
     endforeach;
     ?>
-
-    <!--<div id="contact">-->
-    <!--<div class="content">-->
-    <!--<p>-->
-    <!--Shoot me a note! <a class="inline" href="#top" onclick="leavingContact();">Click here to go back</a>.-->
-    <!--</p>-->
-    <!--<div id="form">-->
-    <!--<form id="ajax-contact-form" action="javascript:alert('success!');">-->
-    <!--<fieldset class="info_fieldset">-->
-    <!--<div id="note"></div>-->
-    <!--<div id="fields">-->
-    <!--<label for="name">Name</label>-->
-    <!--<input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" /><br />-->
-    <!--<label for="email">Email</label>-->
-    <!--<input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />-->
-    <!--<label for="comments">Comments</label>-->
-    <!--<textarea id="comments" class="textbox textbox2" tabindex="3" name="message" rows="10" cols="50" spellcheck="true"></textarea>-->
-    <!--<input class="formSend btn btn-large btn-primary" type="submit" tabindex="4" value="Send Message" />-->
-    <!--<input class="formClear btn btn-large btn-danger" type="reset" tabindex="5" value="Clear" />-->
-    <!--</div>-->
-    <!--</fieldset>-->
-    <!--</form>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>&lt;!&ndash; end contact &ndash;&gt;-->
-    <!--<footer id="footer">-->
-    <!--<div id="quickLinks">-->
-    <!--<p>-->
-    <!--<a href="#" onClick="quickLinks();">Quick links &#187;</a>-->
-    <!--</p>-->
-    <!--</div>-->
 
     <?php
     include ("./blog/wp-content/themes/stumblr/footer.php");
