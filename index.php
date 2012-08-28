59f42ff972675bb202bdefccc8fda7376a167cea
cwoodford <chase@chasewoodford.com>
2012-08-28 15:11:30 -0400

    wp excerpt test

--- index.php
+++ index.php
@@ -50,9 +50,9 @@
     <?php
     $posts = get_posts('numberposts=1&order=DSC&orderby=post_title');
     foreach ($posts as $post) : start_wp(); ?>
-        <?php the_date(); echo "<br />"; ?>
         <?php the_title(); ?>
         <?php the_excerpt(); ?>
+        <?php the_date(); echo "<br />"; ?>
         <?php
     endforeach;
     ?>
