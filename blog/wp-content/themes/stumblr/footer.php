a8aab992e02960a1e3abdde32bace80386cf339a
cwoodford <chase@chasewoodford.com>
2012-09-01 17:20:53 -0400

    js in header test

--- blog/wp-content/themes/stumblr/footer.php
+++ blog/wp-content/themes/stumblr/footer.php
@@ -9,6 +9,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/javascript/jquery-1.7.min.js"><\/script>')</script>
 <script src="/javascript/contact_form.js"></script>
+
 <script src="/javascript/chirp.min.js"></script>
 <script>Chirp({
     user: 'chase1263070', //Twitter username
@@ -19,7 +20,7 @@
     cacheExpire: 1000 * 60 * 2, //Number of milliseconds to cache tweets
     target: 'tweet-container', //Where the tweet gets loaded
     templates: {
-        tweet: '<div class="quote-before">&#8220;</div><div class="tweet"><p>{{html}}</p></div><div class="quote-after">&#8221;</div><div class="attribute">{{time_ago}}&nbsp;via&nbsp;{{source}}</div><div class="twitter-button"><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script><a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a></div>'
+        tweet: '<div class="quote-before">&#8220;</div><div class="tweet"><p>{{html}}</p></div><div class="quote-after">&#8221;</div><div class="attribute">{{time_ago}}&nbsp;via&nbsp;{{source}}</div><div class="twitter-button"><a href="https://twitter.com/chase1263070" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @chase1263070</a></div>'
     }
 })</script>
 <script type="text/javascript">
