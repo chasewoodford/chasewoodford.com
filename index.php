4503b6d04a9ec7218c86d19215d5236e83c58e5c
cwoodford <chase@chasewoodford.com>
2012-09-05 20:47:33 -0400

    adding link back to home

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <div class="wrapper">
     <header class="grid-8">
         <img class="logo pulse" src="images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
-        <h1 class="sitename"><a href="./" title="chasewoodford.com">chasewoodford.com</h1></a>
+        <h1 class="sitename">chasewoodford.com</h1>
         <aside class="search-box">
             <form action="" method="post">
                 <input type="text" name="text" class="search-box-input"/>
