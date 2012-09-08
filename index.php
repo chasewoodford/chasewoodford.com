7b1ea5ed83bf928e66dda1d192aecfe959c4ecce
cwoodford <chase@chasewoodford.com>
2012-09-07 20:09:43 -0400

    wordpress tags test

--- index.php
+++ index.php
@@ -73,8 +73,7 @@
                                     $posttags = get_the_tags();
                                     if ($posttags) {
                                         foreach($posttags as $tag) {
-                                            echo '<a href="';echo bloginfo(url);
-                                            echo '/?tag=' . $tag->slug . '" class="' . $tag->slug . '">' . $tag->name . '</a>';
+                                            echo '<a href="';echo bloginfo(url);echo '/?tag=' . $tag->slug . '" class="no-shadow">' . $tag->name . '</a>';
                                         }
                                     }
                                 ?>
