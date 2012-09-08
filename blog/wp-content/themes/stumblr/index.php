494ffb06e23ddd93c0f31e57d78e0f935e6e2d11
cwoodford <chase@chasewoodford.com>
2012-09-07 21:32:39 -0400

    yeah, this probably won't work

--- blog/wp-content/themes/stumblr/index.php
+++ blog/wp-content/themes/stumblr/index.php
@@ -2,29 +2,39 @@
 
 <div id="post-area">
 <?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>	
+<?php while (have_posts()) : the_post(); ?>
 
-	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    	<div class="title"><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></div>
-			<div class="content">
-            	<?php if ( has_post_thumbnail() ) { ?><div class="stumblr-image"><?php the_post_thumbnail( 'stumblr-large-image' );  ?></div><?php } ?>  
-                 <?php the_content(''); ?>
-             </div>
-            
-            <div class="stumblr-meta">
-                <p>
-                    <span class="stumblr-date"><?php the_time('F Y'); ?></span>
-                    <?php
-                    $posttags = get_the_tags();
-                    if ($posttags) {
-                        foreach($posttags as $tag) {
-                            echo '<a href="';echo bloginfo(url);echo '/?tag=' . $tag->slug . '" class="no-shadow">' . $tag->name . '</a>&nbsp;&nbsp;';
-                        }
-                    }
-                    ?>
-                </p>
-            </div>
-	<div class="clear"></div></div><!-- end post -->
+        <article class="grid-8 hero post type-post">
+            <header>
+                <h3 class="title">
+                    <a class="post-title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                        <span class="blog-category" style="">
+                            <?php the_category(', ') ?>
+                        </span>
+                </h3>
+            </header>
+            <section class="content">
+                <?php the_excerpt(); ?>
+                <div class="stumblr-meta">
+                    <p>
+                            <span class="stumblr-date">
+                                <?php the_time('F Y'); ?>
+                            </span>
+                            <span class="stumblr-category">
+                                <?php
+                                $posttags = get_the_tags();
+                                if ($posttags) {
+                                    foreach($posttags as $tag) {
+                                        echo '<a href="';echo bloginfo(url);echo '/?tag=' . $tag->slug . '" class="no-shadow">' . $tag->name . '</a>&nbsp;&nbsp;';
+                                    }
+                                }
+                                ?>
+                            </span>
+                    </p>
+                </div>
+            </section>
+            <div class="clear"></div>
+        </article>
 
 <?php endwhile; ?>
 
