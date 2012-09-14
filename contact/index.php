2338bce566aebeda9d4fb347a7246e24482a0227
cwoodford <chase@chasewoodford.com>
2012-09-13 20:59:35 -0400

    refactoring

--- contact/index.php
+++ contact/index.php
@@ -22,23 +22,10 @@
             </form>
         </aside>
         <nav class="nav-primary">
-            <ul>
-                <li>
-                    <a class="about" href="../about" title="about"><h2>about</h2></a>
-                </li>
-                <li><h2>&bull;</h2></li>
-                <li>
-                    <a class="work" href="../work" title="work"><h2>work</h2></a>
-                </li>
-                <li><h2>&bull;</h2></li>
-                <li>
-                    <a class="blog" href="../blog" title="blog"><h2>blog</h2></a>
-                </li>
-                <li><h2>&bull;</h2></li>
-                <li>
-                    <a class="contact" href="../contact" title="contact"><h2>contact</h2></a>
-                </li>
-            </ul>
+            <a class="about" href="../about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="work" href="../work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="blog" href="../blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="contact" href="../contact" title="contact"><h2>contact</h2></a>
         </nav>
     </header>
 
