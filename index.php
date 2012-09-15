1af91848683fcb1b667557e74fca12de7eaef812
cwoodford <chase@chasewoodford.com>
2012-09-15 00:12:32 -0400

    adding callouts

--- index.php
+++ index.php
@@ -69,9 +69,10 @@
                         </p>
                     </div>
                 </section>
-                <div class="clear"></div>
+
             </article>
             <div class="recent-rambling"></div>
+            <div class="clear"></div>
         <?php
         endforeach;
         ?>
