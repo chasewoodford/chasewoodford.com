ea03c8aa532bcbbd92d980298fddb2642faf40da
cwoodford <chase@chasewoodford.com>
2012-09-14 22:35:31 -0400

    sticky footer

--- work/index.php
+++ work/index.php
@@ -10,7 +10,7 @@
 </head>
 <body id="top" class="work">
 <div class="wrapper">
-        <div style="padding-bottom: 30px;" class="clearfix">
+    <div class="main-content">
         <header class="grid-8">
             <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
             <h1 class="sitename"><a class="kansas" href="../" title="home">chasewoodford.com</a></h1>
