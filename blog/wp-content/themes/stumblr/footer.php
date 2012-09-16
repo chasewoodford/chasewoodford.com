0438b72c468dfcc1d15e3b6bd6d88ef77ccf29b1
cwoodford <chase@chasewoodford.com>
2012-09-16 11:11:51 -0400

    reverting; lowering tweet retrieval number to decrease load time

--- blog/wp-content/themes/stumblr/footer.php
+++ blog/wp-content/themes/stumblr/footer.php
@@ -11,6 +11,19 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/javascript/jquery-1.7.min.js"><\/script>')</script>
 <script src="/javascript/contact_form.js"></script>
+<script src="/javascript/chirp.min.js"></script>
+<script>Chirp({
+    user: 'chase1263070', //Twitter username
+    max: 1, //Maximum number of tweets to show
+    count: 1, //Total tweets to retrieve
+    retweets: false, //Show/Don't show retweets
+    replies: false,  //Show/Don't show replies
+    cacheExpire: 1000 * 60 * 2, //Number of milliseconds to cache tweets
+    target: 'tweet-container', //Where the tweet gets loaded
+    templates: {
+        tweet: '<div class="quote-before">&#8220;</div><div class="tweet"><p>{{html}}</p></div><div class="quote-after">&#8221;</div><div class="attribute">{{time_ago}}&nbsp;via&nbsp;{{source}}</div><div class="twitter-button"><a href="https://twitter.com/chase1263070" class="twitter-follow-button attribute" data-show-count="false" data-lang="en">Follow @chase1263070</a></div>'
+    }
+})</script>
 <script type="text/javascript">
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-28396921-1']);
