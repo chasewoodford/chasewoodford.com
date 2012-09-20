17655c336f257981901119f99a55f35369fdc7c8
cwoodford <chase@chasewoodford.com>
2012-09-20 19:51:55 -0400

    adding header php file

--- index.php
+++ index.php
@@ -11,25 +11,9 @@
 </head>
 <body id="top">
 <div class="wrapper">
-    <header class="grid-8">
-        <img class="logo pulse" src="images/logo.png" alt="logo"/>
-        <h1 class="sitename">chasewoodford.com</h1>
-        <aside class="search-box">
-            <form action="" method="post">
-                <input type="text" name="text" class="search-box-input"/>
-                <button class="btn search-box-btn">
-                    <i class="icon-search icon-white"></i>
-                </button>
-            </form>
-        </aside>
-        <nav class="nav-primary">
-            <a class="about" href="../about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
-            <a class="work" href="../work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
-            <a class="blog" href="../blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
-            <a class="contact" href="../contact" title="contact"><h2>contact</h2></a>
-        </nav>
-
-    </header>
+    <?php
+    include ("./header.php");
+    ?>
 
     <div class="grid-8">
 
