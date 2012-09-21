fa5a230fce81525f1f5da5741759ba3c151029f5
cwoodford <chase@chasewoodford.com>
2012-09-20 21:13:58 -0400

    contact form test

--- contact/index.php
+++ contact/index.php
@@ -35,14 +35,18 @@
                             <fieldset class="info_fieldset">
                                 <div id="note"></div>
                                 <div id="fields">
-                                    <label for="name">Name</label>
-                                    <input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" /><br />
-                                    <label for="email">Email</label>
-                                    <input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />
-                                    <label for="comments">Comments</label>
-                                    <textarea id="comments" class="textbox textbox2" tabindex="3" name="message" rows="10" cols="50" spellcheck="true"></textarea>
-                                    <input class="formSend btn btn-large btn-primary" type="submit" tabindex="4" value="Send Message" />
-                                    <input class="formClear btn btn-large btn-danger" type="reset" tabindex="5" value="Clear" />
+                                    <div>
+                                        <label for="name">Name</label>
+                                        <input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" /><br />
+                                        <label for="email">Email</label>
+                                        <input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />
+                                    </div>
+                                    <div>
+                                        <label for="comments">Comments</label>
+                                        <textarea id="comments" class="textbox textbox2" tabindex="3" name="message" rows="10" cols="50" spellcheck="true"></textarea>
+                                        <input class="formSend btn btn-large btn-primary" type="submit" tabindex="4" value="Send Message" />
+                                        <input class="formClear btn btn-large btn-danger" type="reset" tabindex="5" value="Clear" />
+                                    </div>
                                 </div>
                             </fieldset>
                         </form>
