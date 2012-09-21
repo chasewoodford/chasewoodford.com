3cefbf34460e60d00612d70d25605545abc72207
cwoodford <chase@chasewoodford.com>
2012-09-20 21:03:44 -0400

    contact form test

--- contact/index.php
+++ contact/index.php
@@ -30,24 +30,22 @@
                     <p>
                         I generally don't take on freelance projects unless they are really awesome. But I'm always happy to chat it up free of charge. Drop me a line!
                     </p>
-                    <div id="contact">
-                        <div id="form">
-                            <form id="ajax-contact-form" action="javascript:alert('success!');">
-                                <fieldset class="info_fieldset">
-                                    <div id="note"></div>
-                                    <div id="fields">
-                                        <label for="name">Name</label>
-                                        <input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" /><br />
-                                        <label for="email">Email</label>
-                                        <input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />
-                                        <label for="comments">Comments</label>
-                                        <textarea id="comments" class="textbox textbox2" tabindex="3" name="message" rows="10" cols="50" spellcheck="true"></textarea>
-                                        <input class="formSend btn btn-large btn-primary" type="submit" tabindex="4" value="Send Message" />
-                                        <input class="formClear btn btn-large btn-danger" type="reset" tabindex="5" value="Clear" />
-                                    </div>
-                                </fieldset>
-                            </form>
-                        </div>
+                    <div>
+                        <form id="ajax-contact-form" action="javascript:alert('Great success!');">
+                            <fieldset class="info_fieldset">
+                                <div id="note"></div>
+                                <div id="fields">
+                                    <label for="name">Name</label>
+                                    <input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" /><br />
+                                    <label for="email">Email</label>
+                                    <input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />
+                                    <label for="comments">Comments</label>
+                                    <textarea id="comments" class="textbox textbox2" tabindex="3" name="message" rows="10" cols="50" spellcheck="true"></textarea>
+                                    <input class="formSend btn btn-large btn-primary" type="submit" tabindex="4" value="Send Message" />
+                                    <input class="formClear btn btn-large btn-danger" type="reset" tabindex="5" value="Clear" />
+                                </div>
+                            </fieldset>
+                        </form>
                     </div>
                     <ul>
                         <li>Link to social media</li>
