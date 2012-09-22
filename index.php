5a22981d192182b9fe52f5d451fefd9d2ca64e3d
cwoodford <chase@chasewoodford.com>
2012-09-22 10:54:04 -0400

    wp category filter test

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         ?>
 
         <?php
-        query_posts( 'cat=4&posts_per_page=1&orderby=title&order=DESC' );
+        query_posts( 'cat=5&posts_per_page=1&orderby=title&order=DESC' );
         while ( have_posts() ) : the_post(); ?>
             <div class="latest-blog"></div>
             <article class="grid-8 hero post type-post">
