5547ed5112d5870b7cb51d42450ff1dffdf91f8d
cwoodford <chase@chasewoodford.com>
2012-10-06 21:23:16 -0400

    adding icons

--- about/index.php
+++ about/index.php
@@ -50,11 +50,11 @@
                         <p class="stumblr-other-right">
                             <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-twitter-bird"></i></a></span>
                             <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-facebook"></i></a></span>
-                            <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-linkedin"></i></a></span>
                             <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-googleplus-rect"></i></a></span>
+                            <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-linkedin"></i></a></span>
                             <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-github-text"></i></a></span>
-                            <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-skype"></i></a></span>
                             <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-lastfm"></i></a></span>
+                            <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-skype"></i></a></span>
                         </p>
                     </div>
                 </section>
