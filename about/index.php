128a8a4e841ec2adba93d1ff5380d965a1dd1e30
cwoodford <chase@chasewoodford.com>
2012-10-06 21:17:50 -0400

    adding icons

--- about/index.php
+++ about/index.php
@@ -48,11 +48,11 @@
                     </ul>
                     <div class="stumblr-meta">
                         <p class="stumblr-other-right">
-                            <span style="margin:0 6px; font-size: 12px;"><a class="no-shadow" href="#"><i class="icon-twitter-bird"></i></a></span>
-                            <span style="margin:0 6px; font-size: 12px;"><a class="no-shadow" href="#"><i class="icon-linkedin"></i></a></span>
-                            <span style="margin:0 6px; font-size: 12px;"><a class="no-shadow" href="#"><i class="icon-lastfm"></i></a></span>
-                            <span style="margin:0 6px; font-size: 12px;"><a class="no-shadow" href="#"><i class="icon-github-text"></i></a></span>
-                            <span style="margin:0 6px; font-size: 12px;"><a class="no-shadow" href="#"><i class="icon-skype"></i></a></span>
+                            <span style="margin:0 6px; font-size: 14px;"><a class="no-shadow" href="#"><i class="icon-twitter-bird"></i></a></span>
+                            <span style="margin:0 6px; font-size: 14px;"><a class="no-shadow" href="#"><i class="icon-linkedin"></i></a></span>
+                            <span style="margin:0 6px; font-size: 14px;"><a class="no-shadow" href="#"><i class="icon-lastfm"></i></a></span>
+                            <span style="margin:0 6px; font-size: 14px;"><a class="no-shadow" href="#"><i class="icon-github-text"></i></a></span>
+                            <span style="margin:0 6px; font-size: 14px;"><a class="no-shadow" href="#"><i class="icon-skype"></i></a></span>
                         </p>
                     </div>
                 </section>
