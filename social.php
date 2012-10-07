193147fecb4c7511606546258b97d78fece67f24
cwoodford <chase@chasewoodford.com>
2012-10-06 21:50:56 -0400

    adding icons

--- social.php
+++ social.php
@@ -1,11 +1,9 @@
 <div class="stumblr-meta">
     <p class="stumblr-other-right">
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-twitter-bird"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-facebook"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-googleplus-rect"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-linkedin"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-github-text"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-lastfm"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="#"><i class="icon-skype"></i></a></span>
+        <span style="margin:0 6px;"><a class="no-shadow" href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"></i></a></span>
+        <span style="margin:0 6px;"><a class="no-shadow" href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"></i></a></span>
+        <span style="margin:0 6px;"><a class="no-shadow" href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"></i></a></span>
+        <span style="margin:0 6px;"><a class="no-shadow" href="https://github.com/chasewoodford" title="github"><i class="icon-github-text"></i></a></span>
+        <span style="margin:0 6px;"><a class="no-shadow" href="http://www.last.fm/music/Chase+Woodford/Long+Story+Short" title="last.fm"><i class="icon-lastfm"></i></a></span>
     </p>
 </div>
