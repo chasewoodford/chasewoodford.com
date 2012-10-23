221853e8f28756f4c7e6d8bf0f8e584ad3b4d06c
cwoodford <chase@chasewoodford.com>
2012-10-22 21:39:45 -0400

    removing img hovers for static content

--- about/index.php
+++ about/index.php
@@ -30,8 +30,8 @@
                     </h3>
                 </header>
                 <section class="content center">
-                    <div class="static main">
-                        <img src="../images/chase.png" alt="Chase sitting at his computer">
+                    <div class="main">
+                        <img class="static" src="../images/chase.png" alt="Chase sitting at his computer">
                     </div>
                     <p class="left">
                         Hi, my name's Chase Woodford. I'm a web designer/developer living and working out of the suburbs of Philadelphia, PA, USA.
