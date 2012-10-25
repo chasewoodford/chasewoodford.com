c3065c3ad724165ec2d6a8d03b0e3276810b3be9
cwoodford <chase@chasewoodford.com>
2012-10-24 22:18:17 -0400

    re-sizing form inputs

--- contact/index.php
+++ contact/index.php
@@ -35,19 +35,19 @@
                             <fieldset class="info_fieldset">
                                 <div id="note"></div>
                                 <div id="fields">
-                                    <div style="display:inline;float:left; width:15%">
+                                    <div style="display:inline;float:left; width:20%">
                                         <label for="name">Name</label>
                                     </div>
                                     <div style="display:inline;float:left; width:80%">
                                         <input id="name" class="textbox" type="text" tabindex="1" name="name" value="" spellcheck="false" autofocus /><br />
                                     </div>
-                                    <div style="display:inline;float:left; width:15%">
+                                    <div style="display:inline;float:left; width:20%">
                                         <label for="email">Email</label>
                                     </div>
                                     <div style="display:inline;float:left; width:80%">
                                         <input id="email" class="textbox" type="email" tabindex="2" name="email" value="" spellcheck="false" /><br />
                                     </div>
-                                    <div style="display:inline;float:left; width:15%">
+                                    <div style="display:inline;float:left; width:20%">
                                         <label for="comments">Comments</label>
                                     </div>
                                     <div style="display:inline;float:left; width:80%">
