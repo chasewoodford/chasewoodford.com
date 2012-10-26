9730ed002a7d49d3e84b7780b164108d01869c6a
cwoodford <chase@chasewoodford.com>
2012-10-26 09:28:00 -0400

    styling links

--- blog/wp-content/themes/stumblr/header.php
+++ blog/wp-content/themes/stumblr/header.php
@@ -16,7 +16,7 @@
 <div class="wrapper">
     <header class="grid-8">
         <img class="logo pulse" src="../../../../images/logo.png" alt="logo"/>
-        <h1 class="sitename"><a class="kansas nav" href="http://www.chasewoodford.com" title="home">chasewoodford.com</a></h1>
+        <h1 class="sitename"><a class="kansas" href="http://www.chasewoodford.com" title="home">chasewoodford.com</a></h1>
         <aside class="search-box">
             <form action="" method="post">
                 <input type="text" name="text" class="search-box-input"/>
@@ -26,9 +26,9 @@
             </form>
         </aside>
         <nav class="nav-primary">
-            <a class="about nav" href="../../../../about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
-            <a class="work nav" href="../../../../work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
-            <a class="blog nav" href="../../../../blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
-            <a class="contact nav" href="../../../../contact" title="contact"><h2>contact</h2></a>
+            <a class="about" href="../../../../about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="work" href="../../../../work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="blog" href="../../../../blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="contact" href="../../../../contact" title="contact"><h2>contact</h2></a>
         </nav>
     </header>
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header class="grid-8">
     <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
-    <h1 class="sitename"><a class="kansas nav" href="../" title="home">chasewoodford.com</a></h1>
+    <h1 class="sitename"><a class="kansas" href="../" title="home">chasewoodford.com</a></h1>
     <aside class="search-box">
         <form action="" method="post">
             <input type="text" name="text" class="search-box-input"/>
@@ -10,9 +10,9 @@
         </form>
     </aside>
     <nav class="nav-primary">
-        <a class="about nav" href="../about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
-        <a class="work nav" href="../work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
-        <a class="blog nav" href="../blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
-        <a class="contact nav" href="../contact" title="contact"><h2>contact</h2></a>
+        <a class="about" href="../about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
+        <a class="work" href="../work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
+        <a class="blog" href="../blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
+        <a class="contact" href="../contact" title="contact"><h2>contact</h2></a>
     </nav>
 </header>
