60bf6d226a4e8edf327dfd55e7b88e9da7c05c24
cwoodford <chase@chasewoodford.com>
2012-10-27 10:16:24 -0400

    updating fontello

--- social.php
+++ social.php
@@ -4,6 +4,7 @@
         <span style="margin:0 6px;"><a class="no-shadow" href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"></i></a></span>
         <span style="margin:0 6px;"><a class="no-shadow" href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"></i></a></span>
         <span style="margin:0 6px;"><a class="no-shadow" href="https://github.com/chasewoodford" title="github"><i class="icon-github"></i></a></span>
-        <span style="margin:0 0 0 6px;"><a class="no-shadow" href="#" title="soundcloud"><i class="icon-cloud"></i></a></span>
+        <span style="margin:0 0 0
+        6px;"><a class="no-shadow" href="#" title="soundcloud"><i class="icon-cloud"></i></a></span>
     </p>
 </div>
