042e0f841c87474c31f0ee344a078c642f9581d1
cwoodford <chase@chasewoodford.com>
2012-11-11 15:56:14 -0500

    renaming theme

---
+++ blog/wp-content/themes/v2/comments.php
@@ -0,0 +1,62 @@
+<?php if ( have_comments() ) : ?>
+
+    <div style="overflow:hidden;">
+
+    <p class="comments">
+      <strong><em>COMMENTS:</em></strong>
+    </p>
+
+    <ul class="comments">
+        <?php wp_list_comments(); ?>
+    </ul>
+	
+<?php else : // this is displayed if there are no comments so far ?>
+
+	<?php if ( comments_open() ) : ?>
+		<!-- If comments are open, but there are no comments. -->
+
+	<?php else : // comments are closed ?>
+	<?php endif; ?>
+	
+<?php endif; ?>
+
+<?php if ( comments_open() ) : ?>
+
+    <div class="cancel-comment-reply">
+        <?php cancel_comment_reply_link(); ?>
+    </div>
+
+    <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+
+        <fieldset class="info_fieldset">
+            <div id="fields">
+                <div style="display:inline;float:left; width:18%">
+                    <label for="author">Name</label>
+                </div>
+                <div style="display:inline;float:left; width:80%">
+                    <input type="text" name="author" id="author" value="" class="textbox" aria-required="true"/>
+                </div>
+                <div style="display:inline;float:left; width:18%">
+                    <label for="email">Email</label>
+                </div>
+                <div style="display:inline;float:left; width:80%">
+                    <input type="text" name="email" id="email" value="" class="textbox" aria-required="true"/>
+                </div>
+                <div style="display:inline;float:left; width:18%">
+                    <label for="comment">Comment</label>
+                </div>
+                <div style="display:inline;float:left; width:80%">
+                    <textarea spellcheck="true" name="comment" id="comment" cols="50" rows="4" class="textbox textbox2"></textarea>
+                </div>
+                <div>
+                    <input name="submit" type="submit" value="submit comment" class="formSend btn btn-large btn-primary">
+                </div>
+            </div>
+        </fieldset>
+
+        <?php comment_id_fields(); ?>
+        <?php do_action('comment_form', $post->ID); ?>
+
+    </form>
+
+<?php endif; ?>
---
+++ blog/wp-content/themes/v2/index.php
@@ -0,0 +1,69 @@
+<?php get_header(); ?>
+
+<div class="grid-8">
+    <div id="post-area" class="grid-blog">
+    <?php if (have_posts()) : ?>
+    <?php
+        if (is_home()) {
+            // add the line below to page that doesn't affect category/archive views
+        query_posts( 'cat=-10&orderby=post_date&order=DESC' ); }
+        while ( have_posts() ) : the_post();
+
+    ?>
+        <article class="hero post type-post">
+            <header>
+                <h3 class="title-background">
+                    <a class="post-title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                        <span class="blog-category" style="">
+                            <?php the_category(', ') ?>
+                        </span>
+                </h3>
+            </header>
+            <section class="content">
+                <?php the_content(''); ?>
+                <div class="v2-meta">
+                    <p>
+                        <span class="v2-date">
+                            <?php   the_time('F Y'); ?>
+                        </span>
+                        <span class="v2-category">
+                            <?php
+                            $posttags = get_the_tags();
+                            if ($posttags) {
+                                foreach($posttags as $tag) {
+                                    echo '<a href="';echo bloginfo(url);echo '/?tag=' . $tag->slug . '" class="no-shadow">' . $tag->name . '</a>&nbsp;&nbsp;';
+                                }
+                            }
+                            ?>
+                        </span>
+                        <span class="v2-other-right">
+                            <a href="<?php comments_link(); ?>" class="no-shadow">Comments:&nbsp;<?php $commentscount = get_comments_number(); echo $commentscount; ?></a>
+                        </span>
+                    </p>
+                </div>
+            </section>
+            <div class="clear"></div>
+        </article>
+
+    <?php endwhile; ?>
+
+    <?php else : ?>
+    <?php endif; ?>
+
+    <?php v2_pagination(); ?>
+    <?php echo paginate_links( $args ) ?>
+
+    </div>
+
+    <div id="sidebar" class="grid-2">
+        <?php if ( is_active_sidebar( 'v2_widgets')) { ?>
+        <div id="sidebar-widget-area">
+            <?php dynamic_sidebar( 'v2_widgets' ); ?>
+        </div>
+        <?php }  ?>
+    </div>
+</div>
+
+<?php
+include ("footer.php");
+?>
