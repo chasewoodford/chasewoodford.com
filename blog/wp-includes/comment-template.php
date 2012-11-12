9becaee83d7299626e8f79afd54063de8850261c
cwoodford <chase@chasewoodford.com>
2012-11-11 19:19:23 -0500

    wp comment styling

--- blog/wp-includes/comment-template.php
+++ blog/wp-includes/comment-template.php
@@ -1350,17 +1350,16 @@ class Walker_Comment extends Walker {
 ?>
 		<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
 		<?php if ( 'div' != $args['style'] ) : ?>
-		<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+		<h3 id="comment-<?php comment_ID() ?>" class="comment-body">
 		<?php endif; ?>
-		<div class="comment-author vcard">
-		<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-		<?php printf(__('<cite class="fn">%s</cite> <span class="says">said:</span>'), get_comment_author_link()) ?>
+            <?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+            <?php printf(__('<cite class="fn">%s</cite> <span class="says">said:</span>'), get_comment_author_link()) ?>
+		</h3>
         <?php comment_text() ?>
-		</div>
-<?php if ($comment->comment_approved == '0') : ?>
-		<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
-		<br />
-<?php endif; ?>
+        <?php if ($comment->comment_approved == '0') : ?>
+		    <em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
+		    <br />
+        <?php endif; ?>
 
 		<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
 			<?php
