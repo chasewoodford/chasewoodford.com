5ff3e1646bb39fa78b963bcbecad06353fe4be07
cwoodford <chase@chasewoodford.com>
2012-11-18 16:15:38 -0500

    refactoring

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         require('./blog/wp-blog-header.php');
         ?>
         <?php
-        query_posts( 'cat=-10&posts_per_page=1&orderby=post_date&order=DESC' );
+        query_posts( 'cat=-10,-51&posts_per_page=1&orderby=post_date&order=DESC' );
         while ( have_posts() ) : the_post();
         ?>
             <article class="grid-8 hero post type-post">
