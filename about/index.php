c27f0b5c5802ccf97f81e53a73717ae1d2f5ec1e
cwoodford <chase@chasewoodford.com>
2012-11-17 20:51:28 -0500

    copy updates

--- about/index.php
+++ about/index.php
@@ -41,17 +41,15 @@
                     <p class="left">
                         I enjoy writing about web design, and throughout this site share my experience as a developer working for a small business at the intersection of Big Data and Big Pharma.
                     </p>
-                    <p class="left lead">
-                        In my spare time...
-                    </p>
                     <p class="left">
-                        I like to compose music, which I link to from the <a class="inline" href="/playground" title="playground" target="_self">playground</a> along with all of my other side projects. I also spend a lot of time reading, mostly about web design and user experience with the occasional book on string theory or building time machines, you know, just to keep in the know.
+                        In my spare time I like to compose music, which I link to from the <a class="inline" href="/playground" title="playground" target="_self">playground</a> along with all of my other side projects. I also spend a lot of time reading, mostly about web design and user experience with the occasional book on string theory or building time machines, you know, just to keep in the know.
                     </p>
-                    <p>
+                    <p class="left lead">
+                        currently reading
                         <?php currently_reading_book(); ?>
                     </p>
                     <p class="left lead">
-                        Way back when...
+                        way back when
                     </p>
                     <p class="left">
                         I graduated from Temple University in 2007 with Summa Cum Laude honors, a bachelor's degree in Advertising and a minor in Sociology. I made the Dean's List a bunch of times, held several high ranking positions as a brother of the Alpha Tau Omega fraternity and was even robbed at gunpoint once. Pretty much your typical college experience.
