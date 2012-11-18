db68053f5bd2c48ed7e54f8630e31cbc06ac8b67
cwoodford <chase@chasewoodford.com>
2012-11-18 13:59:50 -0500

    wp plugin test

--- about/index.php
+++ about/index.php
@@ -47,9 +47,10 @@
                     </p>
                     <p class="left">
 <!--                        --><?php //currently_reading_book(); ?>
+                        [AMAZONPRODUCT=1594202850]
                         <?php include ('../blog/wp-content/plugins/amazon-product-in-a-post-plugin/amazon-product-in-a-post.php'); ?>
 
-                        [AMAZONPRODUCT=1594202850]
+
                     </p>
                     <p class="left lead">
                         { way back when }
