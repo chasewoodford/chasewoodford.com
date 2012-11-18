466742ebc243fe0ee0e94c00c83fd6d30d394492
cwoodford <chase@chasewoodford.com>
2012-11-18 14:48:59 -0500

    wp plugin test

--- about/index.php
+++ about/index.php
@@ -48,7 +48,7 @@
                     <p class="left">
                         <?php
                         $post_id = "171";
-                        echo get_post_field('post_content', $post_id, 'edit'); ?>
+                        echo get_post_field('post_content', $post_id, 'attribute'); ?>
                     </p>
                     <p class="left lead">
                         { way back when }
