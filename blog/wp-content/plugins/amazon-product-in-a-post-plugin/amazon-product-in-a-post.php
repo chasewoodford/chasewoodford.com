3f55401db42f70db78868a2ff86374793bae21b7
cwoodford <chase@chasewoodford.com>
2012-11-18 18:16:34 -0500

    customizing plugin

--- blog/wp-content/plugins/amazon-product-in-a-post-plugin/amazon-product-in-a-post.php
+++ blog/wp-content/plugins/amazon-product-in-a-post-plugin/amazon-product-in-a-post.php
@@ -325,11 +325,9 @@ Adapted By: Chase Woodford
 				//$returnval .= '				<a target="amazon-image" href="javascript: void(0)" onclick="artwindow=window.open(\'' .$result['LargeImage'] .'\',\'art\',\'directories=no, location=no, menubar=no, resizable=no, scrollbars=no, status=no, toolbar=no, width=400,height=525\');artwindow.focus();return false;"><span class="amazon-tiny">'.$appip_text_lgimage.'</span></a>'."\n";
 				$returnval .= '					<a rel="appiplightbox" href="'.$result['LargeImage'] .'"></a>'."\n";
 				}
-                $returnval .= '				    <p class="recently-consumed-post-text">'."\n";
                 if($extratext!=''){
                     $returnval .= 				    $extratext;
                 }
-                $returnval .= '				    </p>'."\n";
                 $returnval .= '				</div>'."\n";
 				return $returnval;
 			}
