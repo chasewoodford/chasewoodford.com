195fa9c97fa7b244b944942cf94713d81729040e
cwoodford <chase@chasewoodford.com>
2012-12-22 15:15:48 -0500

    restarting design

--- header.php
+++ header.php
@@ -1,18 +1,34 @@
-<header class="grid-8">
-    <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
-    <h1 class="sitename"><a class="kansas" href="/" title="home">chasewoodford.com</a></h1>
-    <aside class="search-box">
-        <form action="/search-results/">
-            <input type="text" name="q" class="search-box-input"/>
-            <button class="btn search-box-btn">
-                <i class="icon-search-1"></i>
-            </button>
-        </form>
-    </aside>
-    <nav class="nav-primary">
-        <a class="about" href="/about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
-        <a class="work" href="/work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
-        <a class="blog" href="/blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
-        <a class="contact" href="/contact" title="contact"><h2>contact</h2></a>
-    </nav>
-</header>
+<!--<header class="grid-8">-->
+<!--    <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>-->
+<!--    <h1 class="sitename"><a class="kansas" href="/" title="home">chasewoodford.com</a></h1>-->
+<!--    <aside class="search-box">-->
+<!--        <form action="/search-results/">-->
+<!--            <input type="text" name="q" class="search-box-input"/>-->
+<!--            <button class="btn search-box-btn">-->
+<!--                <i class="icon-search-1"></i>-->
+<!--            </button>-->
+<!--        </form>-->
+<!--    </aside>-->
+<!--    <nav class="nav-primary">-->
+<!--        <a class="about" href="/about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>-->
+<!--        <a class="work" href="/work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>-->
+<!--        <a class="blog" href="/blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>-->
+<!--        <a class="contact" href="/contact" title="contact"><h2>contact</h2></a>-->
+<!--    </nav>-->
+<!--</header>-->
+<div class="" id="header">
+    <div id="hdr_inner">
+        <div class="links">
+            <a href="#">One</a> <a href="#">Two</a> <a href=
+                                                           "#">Three</a> <a href="#">Four</a>
+        </div>
+
+        <div class="logo_intuniv"></div>
+
+        <div class="safety">
+            <p>Lorem Ipsum.</p>
+        </div>
+    </div>
+
+    <div class="clearBoth"></div>
+</div>
