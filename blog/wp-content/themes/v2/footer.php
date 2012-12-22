1dfc6ea524470274460c8a05d98b5be353ffaaaf
cwoodford <chase@chasewoodford.com>
2012-12-22 15:48:38 -0500

    restarting design

--- blog/wp-content/themes/v2/footer.php
+++ blog/wp-content/themes/v2/footer.php
@@ -96,6 +96,7 @@
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/javascript/jquery-1.7.min.js"><\/script>')</script>
+<script src="/javascript/common.js" type="text/javascript"></script>
 <script src="/javascript/prism.js"></script>
 <script src="/javascript/contact_form.js"></script>
 <script src="/javascript/chirp.min.js"></script>
