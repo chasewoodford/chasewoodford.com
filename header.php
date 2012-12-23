3b5f160d36a8264f4803172e5f13b9d558bb5a3a
cwoodford <chase@chasewoodford.com>
2012-12-22 21:08:06 -0500

    restarting design

--- header.php
+++ header.php
@@ -4,16 +4,15 @@
             <a class="header-link" href="#">One</a><span class="bullet">&bull;</span>
             <a class="header-link" href="#">Two</a><span class="bullet">&bull;</span>
             <a class="header-link" href="#">Three</a><span class="bullet">&bull;</span>
-            <a class="header-link" href="#">Four</a>
+            <a class="header-link" href="/contact" title="contact">Contact</a>
         </div>
 
         <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
 
-        <nav class="nav-primary">
+        <nav class="nav-primary" style="margin-top: 7px; margin-left: 75px;">
             <a class="nav-link about" href="/about" title="about"><span class="h2">about</span></a>
             <a class="nav-link work" href="/work" title="work"><span class="h2">work</span></a>
             <a class="nav-link blog" href="/blog" title="blog"><span class="h2">blog</span></a>
-            <a class="nav-link contact" href="/contact" title="contact"><span class="h2">contact</span></a>
         </nav>
 
         <aside class="search-box">
