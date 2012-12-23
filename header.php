396b0016af8b159cbc788d37458ae778a9ccc78b
cwoodford <chase@chasewoodford.com>
2012-12-22 20:26:24 -0500

    restarting design

--- header.php
+++ header.php
@@ -9,8 +9,6 @@
 
         <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
 
-        <h1 class="sitename"><a class="kansas" href="/" title="home">chasewoodford.com</a></h1>
-
         <aside class="search-box">
             <form action="/search-results/">
                 <input type="text" name="q" class="search-box-input"/>
@@ -20,14 +18,13 @@
             </form>
         </aside>
 
-        <div class="safety">
-            <nav class="nav-primary">
-                <a class="about" href="/about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
-                <a class="work" href="/work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
-                <a class="blog" href="/blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
-                <a class="contact" href="/contact" title="contact"><h2>contact</h2></a>
-            </nav>
-        </div>
+        <nav class="nav-primary">
+            <a class="about" href="/about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="work" href="/work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="blog" href="/blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
+            <a class="contact" href="/contact" title="contact"><h2>contact</h2></a>
+        </nav>
+
     </div>
 
     <div class="clearBoth"></div>
