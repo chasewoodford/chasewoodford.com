632f81ec0786b859f0545d17065fb66475711171
cwoodford <chase@chasewoodford.com>
2012-12-22 20:17:11 -0500

    restarting design

--- header.php
+++ header.php
@@ -7,9 +7,7 @@
             <a class="header-links" href="#">Four</a>
         </div>
 
-        <div class="logo">
-            <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
-        </div>
+        <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
 
         <h1 class="sitename"><a class="kansas" href="/" title="home">chasewoodford.com</a></h1>
 
