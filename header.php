fb3d5547ee29b3291f261115adbb41da6d7d3331
cwoodford <chase@chasewoodford.com>
2012-12-23 11:28:55 -0500

    redesigning search

--- header.php
+++ header.php
@@ -1,26 +1,26 @@
 <div id="header">
     <div id="hdr_inner" class="grid-8">
         <div class="links">
-            <a class="header-link" href="/" title="home">home</a><span class="bullet">&bull;</span>
-            <a class="header-link" href="#">resume</a><span class="bullet">&bull;</span>
-            <a class="header-link" href="/contact" title="contact">contact</a>
+            <a class="header-link" href="/" title="home" tabindex="6">home</a><span class="bullet">&bull;</span>
+            <a class="header-link" href="#" tabindex="7">resume</a><span class="bullet">&bull;</span>
+            <a class="header-link" href="/contact" title="contact" tabindex="8">contact</a>
         </div>
 
         <!--<img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>-->
 
         <aside class="search-box">
             <form class="quick-search" action="/search-results/">
-                <input type="text" name="q" class="search-box-input"/>
-                <button class="btn search-box-btn">
+                <input type="text" name="q" class="search-box-input" tabindex="4"/>
+                <button class="btn search-box-btn" tabindex="5">
                     <i class="icon-search-1"></i>
                 </button>
             </form>
         </aside>
 
         <nav class="nav-primary">
-            <a class="nav-link no-decoration about" href="/about" title="about"><span class="h2">about</span></a>
-            <a class="nav-link no-decoration work" href="/work" title="work"><span class="h2">work</span></a>
-            <a class="nav-link no-decoration blog" href="/blog" title="blog"><span class="h2">blog</span></a>
+            <a class="nav-link no-decoration about" href="/about" title="about" tabindex="1"><span class="h2">about</span></a>
+            <a class="nav-link no-decoration work" href="/work" title="work" tabindex="2"><span class="h2">work</span></a>
+            <a class="nav-link no-decoration blog" href="/blog" title="blog" tabindex="3"><span class="h2">blog</span></a>
         </nav>
 
     </div>
