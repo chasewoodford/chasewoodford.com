abb376a4c0c54b8f7326c5ac3982b54535549c40
cwoodford <chase@chasewoodford.com>
2012-12-22 20:08:52 -0500

    restarting design

--- header.php
+++ header.php
@@ -1,22 +1,4 @@
-<!--<header class="grid-8">-->
-<!--    <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>-->
-<!--    <h1 class="sitename"><a class="kansas" href="/" title="home">chasewoodford.com</a></h1>-->
-<!--    <aside class="search-box">-->
-<!--        <form action="/search-results/">-->
-<!--            <input type="text" name="q" class="search-box-input"/>-->
-<!--            <button class="btn search-box-btn">-->
-<!--                <i class="icon-search-1"></i>-->
-<!--            </button>-->
-<!--        </form>-->
-<!--    </aside>-->
-<!--    <nav class="nav-primary">-->
-<!--        <a class="about" href="/about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>-->
-<!--        <a class="work" href="/work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>-->
-<!--        <a class="blog" href="/blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>-->
-<!--        <a class="contact" href="/contact" title="contact"><h2>contact</h2></a>-->
-<!--    </nav>-->
-<!--</header>-->
-<div class="" id="header">
+<div class="grid-8" id="header">
     <div id="hdr_inner">
         <div class="links">
             <a class="header-links" href="#">One</a><span class="bullet">&bull;</span>
@@ -25,10 +7,28 @@
             <a class="header-links" href="#">Four</a>
         </div>
 
-        <div class="logo"></div>
+        <div class="logo">
+            <img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>
+        </div>
+
+        <h1 class="sitename"><a class="kansas" href="/" title="home">chasewoodford.com</a></h1>
+
+        <aside class="search-box">
+            <form action="/search-results/">
+                <input type="text" name="q" class="search-box-input"/>
+                <button class="btn search-box-btn">
+                    <i class="icon-search-1"></i>
+                </button>
+            </form>
+        </aside>
 
         <div class="safety">
-            <p>Lorem Ipsum.</p>
+            <nav class="nav-primary">
+                <a class="about" href="/about" title="about"><h2>about</h2></a><h2 class="bullet">&bull;</h2>
+                <a class="work" href="/work" title="work"><h2>work</h2></a><h2 class="bullet">&bull;</h2>
+                <a class="blog" href="/blog" title="blog"><h2>blog</h2></a><h2 class="bullet">&bull;</h2>
+                <a class="contact" href="/contact" title="contact"><h2>contact</h2></a>
+            </nav>
         </div>
     </div>
 
