c68128403a53ed02a237e4dd726edf775aa89e6c
cwoodford <chase@chasewoodford.com>
2012-12-28 19:57:33 -0500

    removing unused classes

--- social.php
+++ social.php
@@ -1,10 +1,9 @@
 <div class="v2-meta">
     <p class="v2-other-right">
-        <span style="margin:0 6px;"><a class="no-shadow" href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"></i></a></span>
-        <span style="margin:0 6px;"><a class="no-shadow" href="https://github.com/chasewoodford" title="github"><i class="icon-github"></i></a></span>
-        <span style="margin:0 0 0
-        6px;"><a class="no-shadow" href="#" title="soundcloud"><i class="icon-cloud"></i></a></span>
+        <span style="margin:0 6px;"><a href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"></i></a></span>
+        <span style="margin:0 6px;"><a href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"></i></a></span>
+        <span style="margin:0 6px;"><a href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"></i></a></span>
+        <span style="margin:0 6px;"><a href="https://github.com/chasewoodford" title="github"><i class="icon-github"></i></a></span>
+        <span style="margin:0 0 0 6px;"><a href="#" title="soundcloud"><i class="icon-cloud"></i></a></span>
     </p>
 </div>
