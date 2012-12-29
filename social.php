7f99bf08304363e0bfe984e3dd3235a579365951
cwoodford <chase@chasewoodford.com>
2012-12-28 22:43:00 -0500

    layout update to social include

--- social.php
+++ social.php
@@ -1,7 +1,7 @@
-<div class="grid-6">
-    <span><a href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"><span class="hide-text">Twitter</span></i></a></span>
-    <span><a href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"><span class="hide-text">Facebook</span></i></a></span>
-    <span><a href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"><span class="hide-text">LinkedIn</span></i></a></span>
-    <span><a href="https://github.com/chasewoodford" title="github"><i class="icon-github"><span class="hide-text">Github</span></i></a></span>
-    <span><a href="#" title="soundcloud"><i class="icon-cloud"><span class="hide-text">Sound Cloud</span></i></a></span>
+<div class="grid-6" style="display:table;">
+    <span style="display:table-cell"><a href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"><span class="hide-text">Twitter</span></i></a></span>
+    <span style="display:table-cell"><a href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"><span class="hide-text">Facebook</span></i></a></span>
+    <span style="display:table-cell"><a href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"><span class="hide-text">LinkedIn</span></i></a></span>
+    <span style="display:table-cell"><a href="https://github.com/chasewoodford" title="github"><i class="icon-github"><span class="hide-text">Github</span></i></a></span>
+    <span style="display:table-cell"><a href="#" title="soundcloud"><i class="icon-cloud"><span class="hide-text">Sound Cloud</span></i></a></span>
 </div>
