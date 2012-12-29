5b50df100ea722f72a7653315bfb45f5542e6f5a
cwoodford <chase@chasewoodford.com>
2012-12-28 22:45:13 -0500

    layout update to social include

--- social.php
+++ social.php
@@ -1,4 +1,4 @@
-<div class="grid-6" style="display:table;">
+<div class="grid-6" style="display:table;text-align:center;">
     <span style="display:table-cell"><a href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"><span class="hide-text">Twitter</span></i></a></span>
     <span style="display:table-cell"><a href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"><span class="hide-text">Facebook</span></i></a></span>
     <span style="display:table-cell"><a href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"><span class="hide-text">LinkedIn</span></i></a></span>
