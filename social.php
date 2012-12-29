f833e5fb5af0a50437b7c2042bddabc56de288a8
cwoodford <chase@chasewoodford.com>
2012-12-28 22:28:34 -0500

    refactoring wells

--- social.php
+++ social.php
@@ -1,9 +1,7 @@
-<div class="v2-meta">
-    <p class="v2-other-right">
-        <span style="margin:0 6px;"><a href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"><span class="hide-text">Twitter</span></i></a></span>
-        <span style="margin:0 6px;"><a href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"><span class="hide-text">Facebook</span></i></a></span>
-        <span style="margin:0 6px;"><a href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"><span class="hide-text">LinkedIn</span></i></a></span>
-        <span style="margin:0 6px;"><a href="https://github.com/chasewoodford" title="github"><i class="icon-github"><span class="hide-text">Github</span></i></a></span>
-        <span style="margin:0 0 0 6px;"><a href="#" title="soundcloud"><i class="icon-cloud"><span class="hide-text">Sound Cloud</span></i></a></span>
-    </p>
+<div class="well">
+    <span style="margin:0 6px;"><a href="https://twitter.com/chase1263070" title="twitter"><i class="icon-twitter-bird"><span class="hide-text">Twitter</span></i></a></span>
+    <span style="margin:0 6px;"><a href="https://www.facebook.com/chasewoodford" title="facebook"><i class="icon-facebook"><span class="hide-text">Facebook</span></i></a></span>
+    <span style="margin:0 6px;"><a href="http://www.linkedin.com/in/chasewoodford" title="linkedin"><i class="icon-linkedin"><span class="hide-text">LinkedIn</span></i></a></span>
+    <span style="margin:0 6px;"><a href="https://github.com/chasewoodford" title="github"><i class="icon-github"><span class="hide-text">Github</span></i></a></span>
+    <span style="margin:0 0 0 6px;"><a href="#" title="soundcloud"><i class="icon-cloud"><span class="hide-text">Sound Cloud</span></i></a></span>
 </div>
