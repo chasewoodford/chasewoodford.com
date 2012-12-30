8885ac978fa82ce8887898fc5a7da0781e5b3569
cwoodford <chase@chasewoodford.com>
2012-12-29 21:12:45 -0500

    test

--- blog/wp-content/themes/v2/index.php
+++ blog/wp-content/themes/v2/index.php
@@ -1,18 +1,3 @@
-<?php
-/**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
- *
- * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
- *
- * @package 	WordPress
- * @subpackage 	Starkers
- * @since 		Starkers 4.0
- */
-?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ): ?>
