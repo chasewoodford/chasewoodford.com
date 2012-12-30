1aacb8574b3a8e89e17d4e65c0a99b34a6087c22
cwoodford <chase@chasewoodford.com>
2012-12-29 21:25:27 -0500

    test

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -5,6 +5,6 @@
 	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
 
 	function starkers_script_enqueuer() {
-		wp_register_style( 'screen', '../../../../../css/compiled/style.css', '', '', 'screen' );
+		wp_register_style( 'screen', '../../../../css/compiled/style.css', '', '', 'screen' );
         wp_enqueue_style( 'screen' );
 	}
