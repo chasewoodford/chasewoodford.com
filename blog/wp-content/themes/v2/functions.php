d0f89a00653e8e275a0eb87a32f68f931eb8875a
cwoodford <chase@chasewoodford.com>
2012-12-30 09:52:08 -0500

    adding custom functions

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -8,3 +8,10 @@
 		wp_register_style( 'screen', '../../../../../css/compiled/style.css', '', '', 'screen' );
         wp_enqueue_style( 'screen' );
 	}
+
+    // Custom read more link after excerpt
+    function new_excerpt_more($more) {
+        global $post;
+        return '... <br/><a class="no-shadow continue-reading inline" href="'. get_permalink($post->ID) . '" title="View full post">continue reading &#187;</a>';
+    }
+    add_filter('excerpt_more', 'new_excerpt_more');
