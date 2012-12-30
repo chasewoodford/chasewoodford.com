9e475615a1cfa3441b7081a452ff772f4be4fcfa
cwoodford <chase@chasewoodford.com>
2012-12-30 10:03:22 -0500

    wp test

--- blog/wp-content/themes/v2/header.php
+++ blog/wp-content/themes/v2/header.php
@@ -20,7 +20,29 @@
 </head>
 <body id="top" <?php body_class(); ?>>
 <div class="wrapper">
-    <?php
-    include ('http://www.chasewoodford.com/header.php');
-    ?>
+    <div id="header" role="banner">
+        <div class="grid-8 header-inner">
+            <div class="links" role="navigation">
+                <a class="header-link" href="/" title="home" tabindex="6">home</a><span class="bullet">&bull;</span>
+                <a class="header-link" href="#" tabindex="7">resume</a><span class="bullet">&bull;</span>
+                <a class="header-link" href="/contact" title="contact" tabindex="8">contact</a>
+            </div>
+            <!--<img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>-->
+            <aside class="right search-box" role="search">
+                <form class="quick-search" action="/search-results/">
+                    <input type="text" name="q" class="search-box-input" tabindex="4"/>
+                    <button class="btn search-box-btn" tabindex="5">
+                        <i class="icon-search-1">
+                            <span class="hide-text">Search</span>
+                        </i>
+                    </button>
+                </form>
+            </aside>
+            <nav class="nav-primary" role="navigation">
+                <a class="nav-link no-decoration about" href="/about" title="about" tabindex="1"><span class="h2">about</span></a>
+                <a class="nav-link no-decoration work" href="/work" title="work" tabindex="2"><span class="h2">work</span></a>
+                <a class="nav-link no-decoration blog" href="/blog" title="blog" tabindex="3"><span class="h2">blog</span></a>
+            </nav>
+        </div>
+    </div>
     <article class="grid-8">
