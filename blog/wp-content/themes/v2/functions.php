f5e35f8b69283ac0bc94e56e178f89d5d8e790ab
cwoodford <chase@chasewoodford.com>
2012-12-29 20:02:21 -0500

    test

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -95,10 +95,3 @@
 			</article>
 		<?php endif;
 	}
-
-    // Custom read more link after excerpt
-    function new_excerpt_more($more) {
-        global $post;
-        return '... <br/><a class="continue-reading" href="'. get_permalink($post->ID) . '" title="View full post">continue reading &#187;</a>';
-    }
-    add_filter('excerpt_more', 'new_excerpt_more');
