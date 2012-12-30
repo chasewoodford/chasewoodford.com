fadfe13fbcfd5c6d30417de1e5c591988b0ccc68
cwoodford <chase@chasewoodford.com>
2012-12-29 21:15:05 -0500

    test

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -1,87 +1,87 @@
 <?php
-//	/**
-//	 * Starkers functions and definitions
-//	 *
-//	 * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
-//	 *
-// 	 * @package 	WordPress
-// 	 * @subpackage 	Starkers
-// 	 * @since 		Starkers 4.0
-//	 */
-//
-//	/* ========================================================================================================================
-//
-//	Required external files
-//
-//	======================================================================================================================== */
-//
+	/**
+	 * Starkers functions and definitions
+	 *
+	 * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+	 *
+ 	 * @package 	WordPress
+ 	 * @subpackage 	Starkers
+ 	 * @since 		Starkers 4.0
+	 */
+
+	/* ========================================================================================================================
+
+	Required external files
+
+	======================================================================================================================== */
+
 //	require_once( 'external/starkers-utilities.php' );
-//
-//	/* ========================================================================================================================
-//
-//	Theme specific settings
-//
-//	Uncomment register_nav_menus to enable a single menu with the title of "Primary Navigation" in your theme
-//
-//	======================================================================================================================== */
-//
+
+	/* ========================================================================================================================
+
+	Theme specific settings
+
+	Uncomment register_nav_menus to enable a single menu with the title of "Primary Navigation" in your theme
+
+	======================================================================================================================== */
+
 //	add_theme_support('post-thumbnails');
-//
-//	// register_nav_menus(array('primary' => 'Primary Navigation'));
-//
-//	/* ========================================================================================================================
-//
-//	Actions and Filters
-//
-//	======================================================================================================================== */
-//
+
+	// register_nav_menus(array('primary' => 'Primary Navigation'));
+
+	/* ========================================================================================================================
+
+	Actions and Filters
+
+	======================================================================================================================== */
+
 //	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
-//
+
 //	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
-//
-//	/* ========================================================================================================================
-//
-//	Custom Post Types - include custom post types and taxonimies here e.g.
-//
-//	e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
-//
-//	======================================================================================================================== */
-//
-//
-//
-//	/* ========================================================================================================================
-//
-//	Scripts
-//
-//	======================================================================================================================== */
-//
-//	/**
-//	 * Add scripts via wp_head()
-//	 *
-//	 * @return void
-//	 * @author Keir Whitaker
-//	 */
-//
-//	function starkers_script_enqueuer() {
+
+	/* ========================================================================================================================
+
+	Custom Post Types - include custom post types and taxonimies here e.g.
+
+	e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
+
+	======================================================================================================================== */
+
+
+
+	/* ========================================================================================================================
+
+	Scripts
+
+	======================================================================================================================== */
+
+	/**
+	 * Add scripts via wp_head()
+	 *
+	 * @return void
+	 * @author Keir Whitaker
+	 */
+
+	function starkers_script_enqueuer() {
 //		wp_register_script( 'site', get_template_directory_uri().'/js/site.js', array( 'jquery' ) );
 //		wp_enqueue_script( 'site' );
-//
-//		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
-//        wp_enqueue_style( 'screen' );
-//	}
-//
-//	/* ========================================================================================================================
-//
-//	Comments
-//
-//	======================================================================================================================== */
-//
-//	/**
-//	 * Custom callback for outputting comments
-//	 *
-//	 * @return void
-//	 * @author Keir Whitaker
-//	 */
+
+		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
+        wp_enqueue_style( 'screen' );
+	}
+
+	/* ========================================================================================================================
+
+	Comments
+
+	======================================================================================================================== */
+
+	/**
+	 * Custom callback for outputting comments
+	 *
+	 * @return void
+	 * @author Keir Whitaker
+	 */
 //	function starkers_comment( $comment, $args, $depth ) {
 //		$GLOBALS['comment'] = $comment;
 //		?>
