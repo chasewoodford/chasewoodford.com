6d40b7674dddeb2664efc07325239110e5a99aa2
cwoodford <chase@chasewoodford.com>
2012-12-29 21:19:12 -0500

    test

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -25,7 +25,7 @@
 
 	======================================================================================================================== */
 
-	add_theme_support('post-thumbnails');
+//	add_theme_support('post-thumbnails');
 
 	// register_nav_menus(array('primary' => 'Primary Navigation'));
 
@@ -35,9 +35,9 @@
 
 	======================================================================================================================== */
 
-	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
+//	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
 
-	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
+//	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
 
 	/* ========================================================================================================================
 
@@ -63,8 +63,8 @@
 	 */
 
 	function starkers_script_enqueuer() {
-		wp_register_script( 'site', get_template_directory_uri().'/js/site.js', array( 'jquery' ) );
-		wp_enqueue_script( 'site' );
+//		wp_register_script( 'site', get_template_directory_uri().'/js/site.js', array( 'jquery' ) );
+//		wp_enqueue_script( 'site' );
 
 		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
         wp_enqueue_style( 'screen' );
@@ -82,16 +82,16 @@
 	 * @return void
 	 * @author Keir Whitaker
 	 */
-	function starkers_comment( $comment, $args, $depth ) {
-		$GLOBALS['comment'] = $comment;
-		?>
-		<?php if ( $comment->comment_approved == '1' ): ?>
-		<li>
-			<article id="comment-<?php comment_ID() ?>">
-				<?php echo get_avatar( $comment ); ?>
-				<h4><?php comment_author_link() ?></h4>
-				<time><a href="#comment-<?php comment_ID() ?>" pubdate><?php comment_date() ?> at <?php comment_time() ?></a></time>
-				<?php comment_text() ?>
-			</article>
-		<?php endif;
-	}
+//	function starkers_comment( $comment, $args, $depth ) {
+//		$GLOBALS['comment'] = $comment;
+//		?>
+<!--		--><?php //if ( $comment->comment_approved == '1' ): ?>
+<!--		<li>-->
+<!--			<article id="comment---><?php //comment_ID() ?><!--">-->
+<!--				--><?php //echo get_avatar( $comment ); ?>
+<!--				<h4>--><?php //comment_author_link() ?><!--</h4>-->
+<!--				<time><a href="#comment---><?php //comment_ID() ?><!--" pubdate>--><?php //comment_date() ?><!-- at --><?php //comment_time() ?><!--</a></time>-->
+<!--				--><?php //comment_text() ?>
+<!--			</article>-->
+<!--		--><?php //endif;
+//	}
