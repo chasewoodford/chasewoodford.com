c3d0390179691e5a8c87f707c3314879950fda49
cwoodford <chase@chasewoodford.com>
2012-12-30 19:17:26 -0500

    minor layout updates

--- javascript/common.js
+++ javascript/common.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-    if ($(window).scrollTop() > 20) {
+    if ($(window).scrollTop() > 30) {
         $("#header").addClass("scroll");
     } else {
         $("#header").removeClass("scroll");
