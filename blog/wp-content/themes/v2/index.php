a6b0b088e21d6880d6df33c29c42df9c6e01bc33
cwoodford <chase@chasewoodford.com>
2012-12-30 20:36:11 -0500

    begin restyling blog

--- blog/wp-content/themes/v2/index.php
+++ blog/wp-content/themes/v2/index.php
@@ -2,12 +2,12 @@
 
 <div class="grid-8">
     <div id="post-area" class="grid-blog">
-<?php if (have_posts()) : ?>
-    <?php
-    if (is_home()) {
-        query_posts( 'cat=-10&orderby=post_date&order=DESC' ); }
-    while ( have_posts() ) : the_post();
-?>
+        <?php if (have_posts()) : ?>
+        <?php
+        if (is_home()) {
+            query_posts( 'cat=-10&orderby=post_date&order=DESC' ); }
+        while ( have_posts() ) : the_post();
+        ?>
         <article class="hero post type-post">
             <header>
                 <h3 class="title-background">
@@ -47,14 +47,13 @@
             <div class="clear"></div>
         </article>
 
-    <?php endwhile; ?>
+        <?php endwhile; ?>
 
         <?php else : ?>
         <?php endif; ?>
 
         <?php v2_pagination(); ?>
         <?php echo paginate_links( $args ) ?>
-
     </div>
 
     <div id="sidebar" class="grid-2">
@@ -65,30 +64,5 @@
         <?php }  ?>
     </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-        <!--<ol>-->
-<?php //while ( have_posts() ) : the_post(); ?>
-<!--	<li>-->
-<!--		<article>-->
-<!--			<h2><a href="--><?php //esc_url( the_permalink() ); ?><!--" title="Permalink to --><?php //the_title(); ?><!--" rel="bookmark">--><?php //the_title(); ?><!--</a></h2>-->
-<!--			<time datetime="--><?php //the_time( 'Y-m-d' ); ?><!--" pubdate>--><?php //the_date(); ?><!-- --><?php //the_time(); ?><!--</time> --><?php //comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-<!--			--><?php //the_content(); ?>
-<!--		</article>-->
-<!--	</li>-->
-<?php //endwhile; ?>
-<!--</ol>-->
-<?php //else: ?>
-<!--<h2>No posts to display</h2>-->
-<?php //endif; ?>
 
 <?php include ('footer.php') ?>
