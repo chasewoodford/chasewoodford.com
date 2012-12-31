6f8b2db5a48882fb79023d888c4b5dfc48b74cf9
cwoodford <chase@chasewoodford.com>
2012-12-30 21:11:47 -0500

    updates to blog styles

--- blog/wp-content/themes/v2/index.php
+++ blog/wp-content/themes/v2/index.php
@@ -1,14 +1,14 @@
 <?php include('header.php') ?>
 
-<div class="grid-8 hero" role="main">
-    <div id="post-area" class="grid-8 left">
+<div class="grid-8 hero main-content">
+    <div id="post-area" class="grid-8 left" role="main">
         <?php if (have_posts()) : ?>
         <?php
         if (is_home()) {
             query_posts( 'cat=-10&orderby=post_date&order=DESC' ); }
         while ( have_posts() ) : the_post();
         ?>
-        <article class="grid-8 main-content hero clearfix">
+        <article class="hero clearfix">
             <header>
                 <h3>
                     <span class="blog-category clearfix">
