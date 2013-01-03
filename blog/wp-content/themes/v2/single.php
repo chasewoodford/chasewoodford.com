1ff9d8de48c2603c04f94201b544acebef6cc69f
cwoodford <chase@chasewoodford.com>
2013-01-03 08:01:51 -0500

    styling single page post

--- blog/wp-content/themes/v2/single.php
+++ blog/wp-content/themes/v2/single.php
@@ -1,33 +1,60 @@
-<?php
-/**
- * The Template for displaying all single posts
- *
- * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
- *
- * @package 	WordPress
- * @subpackage 	Starkers
- * @since 		Starkers 4.0
- */
-?>
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+<?php include('header.php') ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="grid-8 hero main-content">
+    <div id="post-area" class="grid-8 left" role="main">
 
-<article>
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-	<h2><?php the_title(); ?></h2>
-	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-	<?php the_content(); ?>			
+        <article class="hero clearfix">
+            <header>
+                <h3>
+                    <span class="blog-category clearfix">
+                        <?php the_category(', ') ?>&nbsp;>
+                    </span>
+                    <span class="title-bar"><?php the_title(); ?></span>
+                </h3>
+            </header>
+            <section class="content">
+                <?php the_content(); ?>
+                <div class="clearfix"></div>
+                <div class="metadata">
+                    <span class="left">
+                        <?php the_time('F Y'); ?>
+                    </span>
+                    <span class="left">
+                        <?php
+                        $posttags = get_the_tags();
+                        if ($posttags) {
+                            foreach($posttags as $tag) {
+                                echo '<a href="';echo bloginfo(url);echo '/?tag=' . $tag->slug . '">' . $tag->name . '</a>&nbsp;&nbsp;';
+                            }
+                        }
+                        ?>
+                    </span>
+                    <span class="right">
+                        <?php if (get_comments_number() > 0) { ?>
+                            <a href="<?php comments_link(); ?>">comments:&nbsp;<?php $commentscount = get_comments_number(); echo $commentscount; ?></a>
+                        <?php } else { ?>
+                            <a href="<?php comments_link(); ?>">post a comment</a>
+                        <?php } ?>
+                    </span>
+                </div>
+                <?php comments_template( '', true ); ?>
+            </section>
+        </article>
 
-	<?php if ( get_the_author_meta( 'description' ) ) : ?>
-	<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-	<h3>About <?php echo get_the_author() ; ?></h3>
-	<?php the_author_meta( 'description' ); ?>
-	<?php endif; ?>
+    <?php endwhile; ?>
 
-	<?php comments_template( '', true ); ?>
+    </div>
 
-</article>
-<?php endwhile; ?>
+    <div id="sidebar" class="grid-4 right" role="complementary">
+        <?php if ( is_active_sidebar( 'v2_widgets')) { ?>
+            <div id="sidebar-widget-area" class="sidebar-widget-area">
+                <?php dynamic_sidebar( 'v2_widgets' ); ?>
+            </div>
+        <?php }  ?>
+    </div>
+</div>
 
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
+<!--<time datetime="--><?php //the_time( 'Y-m-d' ); ?><!--" pubdate>--><?php //the_date(); ?><!-- --><?php //the_time(); ?><!--</time> --><?php //comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+<?php include ('footer.php') ?>
