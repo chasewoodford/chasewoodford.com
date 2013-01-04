83f90906f3cda351727eca9af9980933401f5236
cwoodford <chase@chasewoodford.com>
2013-01-04 08:04:15 -0500

    removing metadata again

--- blog/wp-content/themes/v2/archive.php
+++ blog/wp-content/themes/v2/archive.php
@@ -7,37 +7,15 @@
         <article class="hero clearfix">
             <header>
                 <h3>
-                <span class="blog-category clearfix">
-                    <?php the_category(', ') ?>&nbsp;>
-                </span>
+                    <span class="blog-category clearfix">
+                        <?php the_category(', ') ?>&nbsp;>
+                    </span>
                     <a class="title-bar" href="<?php the_permalink() ?>" title="view full post"><?php the_title(); ?></a>
                 </h3>
             </header>
             <section class="content">
-                <?php the_content(''); ?>
+                <?php the_excerpt(); ?>
                 <div class="clearfix"></div>
-                <div class="metadata">
-                    <span class="left">
-                        <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F Y'); ?></time>
-                    </span>
-                    <span class="left">
-                        <?php
-                        $posttags = get_the_tags();
-                        if ($posttags) {
-                            foreach($posttags as $tag) {
-                                echo '<a href="';echo bloginfo(url);echo '/?tag=' . $tag->slug . '">' . $tag->name . '</a>&nbsp;&nbsp;';
-                            }
-                        }
-                        ?>
-                    </span>
-                    <span class="right">
-                        <?php if (get_comments_number() > 0) { ?>
-                            <a href="<?php comments_link(); ?>">comments:&nbsp;<?php $commentscount = get_comments_number(); echo $commentscount; ?></a>
-                        <?php } else { ?>
-                            <a href="<?php comments_link(); ?>">post a comment</a>
-                        <?php } ?>
-                    </span>
-                </div>
             </section>
         </article>
 
