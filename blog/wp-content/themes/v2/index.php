8fb986c6e65bbf829ec7ffdf3488fae9ec905ff0
cwoodford <chase@chasewoodford.com>
2013-01-04 21:12:41 -0500

    test

--- blog/wp-content/themes/v2/index.php
+++ blog/wp-content/themes/v2/index.php
@@ -48,6 +48,9 @@
 
         <?php endwhile; ?>
         <?php endif; ?>
+
+        <?php v2_pagination(); ?>
+        <?php echo paginate_links( $args ) ?>
     </div>
 
     <div id="sidebar" class="grid-4 right" role="complementary">
