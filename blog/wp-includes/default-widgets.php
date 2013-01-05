21e9bb936ab88339561a20a3466edbe807c330d6
cwoodford <chase@chasewoodford.com>
2013-01-04 21:51:03 -0500

    test

--- blog/wp-includes/default-widgets.php
+++ blog/wp-includes/default-widgets.php
@@ -570,7 +570,7 @@ class WP_Widget_Recent_Posts extends WP_Widget {
 		<?php echo $before_widget; ?>
 		<?php if ( $title ) echo $before_title . $title . $after_title; ?>
 		<ul>
-		<?php  while ($r->have_posts()) : $r->the_post(); ?>
+		<?php  while ($r->have_posts() && query_posts("cat=-51") ) : $r->the_post(); ?>
 		<li><a href="<?php the_permalink() ?>" title="<?php echo esc_attr(get_the_title() ? get_the_title() : get_the_ID()); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a></li>
 		<?php endwhile; ?>
 		</ul>
