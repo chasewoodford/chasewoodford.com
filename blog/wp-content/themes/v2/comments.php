27cc74a6033895cabd182150aa6aeaca470a6bde
cwoodford <chase@chasewoodford.com>
2013-01-06 18:26:59 -0500

    testing new comments template

--- blog/wp-content/themes/v2/comments.php
+++ blog/wp-content/themes/v2/comments.php
@@ -1,5 +1,7 @@
+<?php /* The Comments Template — with, er, comments! */ ?>
 <div id="comments">
-    <?php // Run some checks for bots and password protected posts
+    <?php /* Run some checks for bots and password protected posts */ ?>
+    <?php
     $req = get_option('require_name_email'); // Checks if fields are required.
     if ( 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']) )
         die ( 'Please do not load this page directly. Thanks!' );
@@ -7,27 +9,69 @@
     if ( $_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password ) :
     ?>
     <div class="nopassword"><?php _e('This post is password protected. Enter the password to view any comments.', 'your-theme') ?></div>
-</div>
+</div><!-- .comments -->
 <?php
 return;
     endif;
 endif;
 ?>
 
+<?php /* See IF there are comments and do the comments stuff! */ ?>
 <?php if ( have_comments() ) : ?>
 
+    <?php /* Count the number of comments and trackbacks (or pings) */
+    $ping_count = $comment_count = 0;
+    foreach ( $comments as $comment )
+        get_comment_type() == "comment" ? ++$comment_count : ++$ping_count;
+    ?>
+
+    <?php /* IF there are comments, show the comments */ ?>
     <?php if ( ! empty($comments_by_type['comment']) ) : ?>
 
         <div id="comments-list" class="comments">
+            <h3><?php printf($comment_count > 1 ? __('<span>%d</span> Comments', 'your-theme') : __('<span>One</span> Comment', 'your-theme'), $comment_count) ?></h3>
 
+            <?php /* If there are enough comments, build the comment navigation  */ ?>
+            <?php $total_pages = get_comment_pages_count(); if ( $total_pages > 1 ) : ?>
+                <div id="comments-nav-above" class="comments-navigation">
+                    <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
+                </div><!-- #comments-nav-above -->
+            <?php endif; ?>
+
+            <?php /* An ordered list of our custom comments callback, custom_comments(), in functions.php   */ ?>
             <ol>
                 <?php wp_list_comments('type=comment&callback=custom_comments'); ?>
             </ol>
 
-        </div>
+            <?php /* If there are enough comments, build the comment navigation */ ?>
+            <?php $total_pages = get_comment_pages_count(); if ( $total_pages > 1 ) : ?>
+                <div id="comments-nav-below" class="comments-navigation">
+                    <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
+                </div><!-- #comments-nav-below -->
+            <?php endif; ?>
 
-<?php endif ?>
+        </div><!-- #comments-list .comments -->
 
+    <?php endif; /* if ( $comment_count ) */ ?>
+
+    <?php /* If there are trackbacks(pings), show the trackbacks  */ ?>
+    <?php if ( ! empty($comments_by_type['pings']) ) : ?>
+
+        <div id="trackbacks-list" class="comments">
+            <h3><?php printf($ping_count > 1 ? __('<span>%d</span> Trackbacks', 'your-theme') : __('<span>One</span> Trackback', 'your-theme'), $ping_count) ?></h3>
+
+            <?php /* An ordered list of our custom trackbacks callback, custom_pings(), in functions.php   */ ?>
+            <ol>
+                <?php wp_list_comments('type=pings&callback=custom_pings'); ?>
+            </ol>
+
+        </div><!-- #trackbacks-list .comments -->
+
+
+    <?php endif /* if ( $ping_count ) */ ?>
+<?php endif /* if ( $comments ) */ ?>
+
+<?php /* If comments are open, build the respond form */ ?>
 <?php if ( 'open' == $post->comment_status ) : ?>
     <div id="respond">
         <h3><?php comment_form_title( __('Post a Comment', 'your-theme'), __('Post a Reply to %s', 'your-theme') ); ?></h3>
@@ -57,24 +101,24 @@ endif;
                         <div id="form-section-author" class="form-section">
                             <div class="form-label"><label for="author"><?php _e('Name', 'your-theme') ?></label> <?php if ($req) _e('<span class="required">*</span>', 'your-theme') ?></div>
                             <div class="form-input"><input id="author" name="author" type="text" value="<?php echo $comment_author ?>" size="30" maxlength="20" tabindex="3" /></div>
-                        </div>
+                        </div><!-- #form-section-author .form-section -->
 
                         <div id="form-section-email" class="form-section">
                             <div class="form-label"><label for="email"><?php _e('Email', 'your-theme') ?></label> <?php if ($req) _e('<span class="required">*</span>', 'your-theme') ?></div>
                             <div class="form-input"><input id="email" name="email" type="text" value="<?php echo $comment_author_email ?>" size="30" maxlength="50" tabindex="4" /></div>
-                        </div>
+                        </div><!-- #form-section-email .form-section -->
 
                         <div id="form-section-url" class="form-section">
                             <div class="form-label"><label for="url"><?php _e('Website', 'your-theme') ?></label></div>
                             <div class="form-input"><input id="url" name="url" type="text" value="<?php echo $comment_author_url ?>" size="30" maxlength="50" tabindex="5" /></div>
-                        </div>
+                        </div><!-- #form-section-url .form-section -->
 
-                    <?php endif ?>
+                    <?php endif /* if ( $user_ID ) */ ?>
 
                     <div id="form-section-comment" class="form-section">
                         <div class="form-label"><label for="comment"><?php _e('Comment', 'your-theme') ?></label></div>
                         <div class="form-textarea"><textarea id="comment" name="comment" cols="45" rows="8" tabindex="6"></textarea></div>
-                    </div>
+                    </div><!-- #form-section-comment .form-section -->
 
                     <div id="form-allowed-tags" class="form-section">
                         <p><span><?php _e('You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:', 'your-theme') ?></span> <code><?php echo allowed_tags(); ?></code></p>
@@ -86,9 +130,11 @@ endif;
 
                     <?php comment_id_fields(); ?>
 
-                </form>
-            </div>
-        <?php endif ?>
-    </div>
-<?php endif ?>
-<?php endif ?>
+                    <?php /* Just … end everything. We're done here. Close it up. */ ?>
+
+                </form><!-- #commentform -->
+            </div><!-- .formcontainer -->
+        <?php endif /* if ( get_option('comment_registration') && !$user_ID ) */ ?>
+    </div><!-- #respond -->
+<?php endif /* if ( 'open' == $post->comment_status ) */ ?>
+</div><!-- #comments -->
