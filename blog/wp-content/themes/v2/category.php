5dd0e898a39f5bf7b21a2ca42c9828a0e40c173b
cwoodford <chase@chasewoodford.com>
2013-01-05 21:51:26 -0500

    adding php if statement

--- blog/wp-content/themes/v2/category.php
+++ blog/wp-content/themes/v2/category.php
@@ -25,7 +25,7 @@
                             the_excerpt();
                         }
                         ?>
-                        <?php if( category__not_in( '10' ) ) : ?>
+                        <?php if( category__not_in( '10' ) ) { ?>
                         <div class="clearfix"></div>
                         <div class="metadata clearfix">
                             <span class="left pubdate">
@@ -42,7 +42,7 @@
                                 ?>
                             </span>
                         </div>
-                        <?php endif; ?>
+                        <?php } ?>
                     </section>
                 </article>
 
