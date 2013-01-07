4dbf67bcf8a9599078d7cdee0a29df12d1ec492b
cwoodford <chase@chasewoodford.com>
2013-01-06 19:33:06 -0500

    styling comments section

--- blog/wp-content/themes/v2/comments.php
+++ blog/wp-content/themes/v2/comments.php
@@ -29,46 +29,17 @@ endif;
     <?php if ( ! empty($comments_by_type['comment']) ) : ?>
 
         <div id="comments-list" class="comments">
-            <h3><?php printf($comment_count > 1 ? __('<span>%d</span> Comments', 'your-theme') : __('<span>One</span> Comment', 'your-theme'), $comment_count) ?></h3>
-
-            <?php /* If there are enough comments, build the comment navigation  */ ?>
-            <?php $total_pages = get_comment_pages_count(); if ( $total_pages > 1 ) : ?>
-                <div id="comments-nav-above" class="comments-navigation">
-                    <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
-                </div><!-- #comments-nav-above -->
-            <?php endif; ?>
+            <h3 class="lead-font"><?php printf($comment_count > 1 ? __('Comments', 'your-theme') : __('Comment', 'your-theme'), $comment_count) ?></h3>
 
             <?php /* An ordered list of our custom comments callback, custom_comments(), in functions.php   */ ?>
-            <ol>
+            <ol class="comment-list">
                 <?php wp_list_comments('type=comment&callback=custom_comments'); ?>
             </ol>
-
-            <?php /* If there are enough comments, build the comment navigation */ ?>
-            <?php $total_pages = get_comment_pages_count(); if ( $total_pages > 1 ) : ?>
-                <div id="comments-nav-below" class="comments-navigation">
-                    <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
-                </div><!-- #comments-nav-below -->
-            <?php endif; ?>
 
         </div><!-- #comments-list .comments -->
 
     <?php endif; /* if ( $comment_count ) */ ?>
 
-    <?php /* If there are trackbacks(pings), show the trackbacks  */ ?>
-    <?php if ( ! empty($comments_by_type['pings']) ) : ?>
-
-        <div id="trackbacks-list" class="comments">
-            <h3><?php printf($ping_count > 1 ? __('<span>%d</span> Trackbacks', 'your-theme') : __('<span>One</span> Trackback', 'your-theme'), $ping_count) ?></h3>
-
-            <?php /* An ordered list of our custom trackbacks callback, custom_pings(), in functions.php   */ ?>
-            <ol>
-                <?php wp_list_comments('type=pings&callback=custom_pings'); ?>
-            </ol>
-
-        </div><!-- #trackbacks-list .comments -->
-
-
-    <?php endif /* if ( $ping_count ) */ ?>
 <?php endif /* if ( $comments ) */ ?>
 
 <?php /* If comments are open, build the respond form */ ?>
