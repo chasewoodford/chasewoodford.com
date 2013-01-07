82b1b0b2f0a6bfeb78e1f1025bd1ce72f4073ba7
cwoodford <chase@chasewoodford.com>
2013-01-06 19:57:58 -0500

    styling comments section

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -93,3 +93,16 @@
         endif;
         ?>
     <?php }
+
+    // Produces an avatar image with the hCard-compliant photo class
+    function commenter_link() {
+        $commenter = get_comment_author_link();
+        if ( ereg( '<a[^>]* class=[^>]+>', $commenter ) ) {
+            $commenter = ereg_replace( '(<a[^>]* class=[\'"]?)', '\\1url ' , $commenter );
+        } else {
+            $commenter = ereg_replace( '(<a )/', '\\1class="url "' , $commenter );
+        }
+        $avatar_email = get_comment_author_email();
+        $avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar( $avatar_email, 80 ) );
+        echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
+    }
