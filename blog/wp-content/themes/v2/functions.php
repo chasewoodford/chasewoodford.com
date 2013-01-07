fecec122aa4d8745344487175dd6e1a3ce7ab70b
cwoodford <chase@chasewoodford.com>
2013-01-06 20:41:03 -0500

    styling comments section

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -71,9 +71,8 @@
         $GLOBALS['comment_depth'] = $depth;
         ?>
     <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-        <div class="comment-meta"><?php commenter_link() ?><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment"><i class="icon-comment" aria-hidden="true"></i>Permalink</a>', 'your-theme'),
+        <div class="comment-meta"><?php commenter_link() ?><?php printf(__('posted a comment on %1$s<span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment"><i class="icon-comment" aria-hidden="true"></i>&nbsp;Permalink</a>', 'your-theme'),
                 get_comment_date('F j, Y'),
-                get_comment_time(),
                 '#comment-' . get_comment_ID() );
             edit_comment_link(__('Edit', 'your-theme'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
         <div class="comment-author vcard"></div>
