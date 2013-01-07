fa6e4ff6332d5b5da2e487c9e29f9be2f2462ad9
cwoodford <chase@chasewoodford.com>
2013-01-06 20:35:51 -0500

    styling comments section

--- blog/wp-content/themes/v2/functions.php
+++ blog/wp-content/themes/v2/functions.php
@@ -71,12 +71,12 @@
         $GLOBALS['comment_depth'] = $depth;
         ?>
     <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-        <div class="comment-author vcard"><?php commenter_link() ?></div>
         <div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'your-theme'),
-                get_comment_date(),
+                get_comment_date('Y-M-D'),
                 get_comment_time(),
                 '#comment-' . get_comment_ID() );
             edit_comment_link(__('Edit', 'your-theme'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
+        <div class="comment-author vcard"><?php commenter_link() ?></div>
         <?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'your-theme') ?>
         <div class="comment-content">
             <?php comment_text() ?>
