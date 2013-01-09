82d4383ac2a77e20715ce8069b94c59304330d47
cwoodford <chase@chasewoodford.com>
2013-01-08 21:11:53 -0500

    reducing the number of posts displayed on main blog page

--- blog/wp-content/themes/v2/index.php
+++ blog/wp-content/themes/v2/index.php
@@ -5,7 +5,7 @@
         <?php if (have_posts()) : ?>
         <?php
         if ( is_home() ) {
-            query_posts( 'cat=-10&orderby=post_date&order=DESC&posts_per_page=50' );
+            query_posts( 'cat=-10&orderby=post_date&order=DESC&posts_per_page=25' );
         }
             while ( have_posts() ) : the_post();
         ?>
