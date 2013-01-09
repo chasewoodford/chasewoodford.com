3a4276f48595d1d28eba7bc206db36c4d3ec3e8b
cwoodford <chase@chasewoodford.com>
2013-01-08 20:13:05 -0500

    tag list test

--- blog/wp-content/themes/v2/index.php
+++ blog/wp-content/themes/v2/index.php
@@ -66,7 +66,8 @@
                         <?php
                         $tags = get_tags();
                         $wptc = wp_tag_cloud('smallest=13.5&largest=13.5&format=array&unit=px&number=20&echo=0');
-                        foreach( $wptc as $wpt ) echo "<li>" . $wpt . $tag->count . "</li>\n";
+                        foreach( $wptc as $wpt ) echo "<li>" . $wpt ;
+                            foreach( $tags as $tag ) echo $tag->count . "</li>\n";
                         ?>
                     </ul>
                 </div>
