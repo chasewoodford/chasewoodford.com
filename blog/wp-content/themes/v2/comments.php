02ca733dfe9ebc7ec939401f603c755bcc43c7fb
cwoodford <chase@chasewoodford.com>
2013-01-08 19:28:08 -0500

    adjusting form styling

--- blog/wp-content/themes/v2/comments.php
+++ blog/wp-content/themes/v2/comments.php
@@ -68,39 +68,41 @@ endif;
 
                         <p id="comment-notes"><?php if ($req) _e('Required fields are marked <span class="required">*</span>', 'your-theme') ?></p>
 
-                        <div id="form-section-author" class="form-section">
-                            <div style="display:inline;float:left; width:18%" class="form-label"><label for="author"><?php _e('Name', 'your-theme') ?></label> <?php if ($req) _e('<span class="required">*</span>', 'your-theme') ?></div>
-                            <div style="display:inline;float:left; width:80%" class="form-input"><input id="author" name="author" type="text" value="<?php echo $comment_author ?>" size="30" maxlength="20" tabindex="3" /></div>
-                        </div><!-- #form-section-author .form-section -->
+                        <fieldset class="info_fieldset">
+                            <div id="form-section-author" class="form-section">
+                                <div style="display:inline;float:left; width:18%" class="form-label"><label for="author"><?php _e('Name', 'your-theme') ?></label> <?php if ($req) _e('<span class="required">*</span>', 'your-theme') ?></div>
+                                <div style="display:inline;float:left; width:80%" class="form-input"><input id="author" name="author" type="text" value="<?php echo $comment_author ?>" size="30" maxlength="20" tabindex="3" /></div>
+                            </div><!-- #form-section-author .form-section -->
 
-                        <div id="form-section-email" class="form-section">
-                            <div style="display:inline;float:left; width:18%" class="form-label"><label for="email"><?php _e('Email', 'your-theme') ?></label> <?php if ($req) _e('<span class="required">*</span>', 'your-theme') ?></div>
-                            <div style="display:inline;float:left; width:80%" class="form-input"><input id="email" name="email" type="text" value="<?php echo $comment_author_email ?>" size="30" maxlength="50" tabindex="4" /></div>
-                        </div><!-- #form-section-email .form-section -->
+                            <div id="form-section-email" class="form-section">
+                                <div style="display:inline;float:left; width:18%" class="form-label"><label for="email"><?php _e('Email', 'your-theme') ?></label> <?php if ($req) _e('<span class="required">*</span>', 'your-theme') ?></div>
+                                <div style="display:inline;float:left; width:80%" class="form-input"><input id="email" name="email" type="text" value="<?php echo $comment_author_email ?>" size="30" maxlength="50" tabindex="4" /></div>
+                            </div><!-- #form-section-email .form-section -->
 
-                        <div id="form-section-url" class="form-section">
-                            <div style="display:inline;float:left; width:18%" class="form-label"><label for="url"><?php _e('Website', 'your-theme') ?></label></div>
-                            <div style="display:inline;float:left; width:80%" class="form-input"><input id="url" name="url" type="text" value="<?php echo $comment_author_url ?>" size="30" maxlength="50" tabindex="5" /></div>
-                        </div><!-- #form-section-url .form-section -->
+                            <div id="form-section-url" class="form-section">
+                                <div style="display:inline;float:left; width:18%" class="form-label"><label for="url"><?php _e('Website', 'your-theme') ?></label></div>
+                                <div style="display:inline;float:left; width:80%" class="form-input"><input id="url" name="url" type="text" value="<?php echo $comment_author_url ?>" size="30" maxlength="50" tabindex="5" /></div>
+                            </div><!-- #form-section-url .form-section -->
 
                     <?php endif /* if ( $user_ID ) */ ?>
 
-                    <div id="form-section-comment" class="form-section">
-                        <div style="display:inline;float:left; width:18%" class="form-label"><label for="comment"><?php _e('Comment', 'your-theme') ?></label></div>
-                        <div style="display:inline;float:left; width:80%" class="form-textarea"><textarea id="comment" name="comment" cols="45" rows="8" tabindex="6" spellcheck="true"></textarea></div>
-                    </div><!-- #form-section-comment .form-section -->
+                            <div id="form-section-comment" class="form-section">
+                                <div style="display:inline;float:left; width:18%" class="form-label"><label for="comment"><?php _e('Comment', 'your-theme') ?></label></div>
+                                <div style="display:inline;float:left; width:80%" class="form-textarea"><textarea id="comment" name="comment" cols="45" rows="8" tabindex="6" spellcheck="true"></textarea></div>
+                            </div><!-- #form-section-comment .form-section -->
+                        </fieldset>
 
-                    <div id="form-allowed-tags" class="form-section allowed-tags">
-                        <p><span><?php _e('You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:', 'your-theme') ?></span> <code><?php echo allowed_tags(); ?></code></p>
-                    </div>
+                        <div id="form-allowed-tags" class="form-section allowed-tags">
+                            <p><span><?php _e('You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:', 'your-theme') ?></span> <code><?php echo allowed_tags(); ?></code></p>
+                        </div>
 
-                    <?php do_action('comment_form', $post->ID); ?>
+                        <?php do_action('comment_form', $post->ID); ?>
 
-                    <div class="form-submit"><input id="submit" class="right btn btn-large btn-primary" name="submit" type="submit" value="<?php _e('Post Comment', 'your-theme') ?>" tabindex="7" /><input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></div>
+                        <div class="form-submit"><input id="submit" class="right btn btn-large btn-primary" name="submit" type="submit" value="<?php _e('Post Comment', 'your-theme') ?>" tabindex="7" /><input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></div>
 
-                    <?php comment_id_fields(); ?>
+                        <?php comment_id_fields(); ?>
 
-                    <?php /* Just … end everything. We're done here. Close it up. */ ?>
+                        <?php /* Just … end everything. We're done here. Close it up. */ ?>
 
                 </form><!-- #commentform -->
             </div><!-- .formcontainer -->
