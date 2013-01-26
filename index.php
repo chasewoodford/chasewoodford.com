41922096b8ac259c985a1659bafdac8f8957077c
cwoodford <chase@chasewoodford.com>
2013-01-25 21:10:16 -0500

    adding favicon

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 
     <title>Chase Woodford &#124; Designer &amp; Developer</title>
 
+    <link rel="icon"
+          type="image/png"
+          href="/images/logo.png">
+
     <link rel="stylesheet" href="/css/compiled/style.css">
 
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
