36f26842b3ab248d52dfad13d94e53c43ed3cadf
cwoodford <chase@chasewoodford.com>
2013-03-09 21:15:05 -0500

    text adjustment

--- work/index.php
+++ work/index.php
@@ -39,8 +39,9 @@
                         </a>
                     </div>
                     <p class="lead-font text-center">
-                        Verilogue | 2012 Golf Classic | Website
+                        Verilogue / 2012 Golf Classic / Website
                     </p>
+                    <p
                     <p>
                         <!-- TODO: turn into post excerpt -->
                         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi...
