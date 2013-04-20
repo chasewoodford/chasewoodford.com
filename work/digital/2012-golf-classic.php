330d44ef8a8b081c769762cdef71336e3d8bc5ab
cwoodford <chase@chasewoodford.com>
2013-04-20 10:59:09 -0400

    adding images to portfolio page

--- work/digital/2012-golf-classic.php
+++ work/digital/2012-golf-classic.php
@@ -63,10 +63,8 @@
                     <p>
                         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
                     </p>
-                    <div class="blog-image text-center">
-                        <img src="../../images/highlight-2012GolfClassic-001.png" alt=""/>
-                    </div>
                     <p>
+                        <img class="clearfix left no-decoration" src="../../images/highlight-2012GolfClassic-001.png" alt=""/>
                         Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis.
                     </p>
                 </section>
