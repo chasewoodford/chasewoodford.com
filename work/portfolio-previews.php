fdc27356e689f2cbfb50b427e8601854f3aaea37
cwoodford <chase@chasewoodford.com>
2013-05-25 11:15:59 -0400

    updating portfolio previews

--- work/portfolio-previews.php
+++ work/portfolio-previews.php
@@ -2,7 +2,17 @@
     <p class="lead"></p>
     <div class="preview">
         <a href="#">
+            <img src="/images/screenshot-phillySportPast.png" width="200" height="100">
+        </a>
+    </div>
+    <div class="preview">
+        <a href="#">
             <img src="/images/screenshot-patientJourney.png" width="200" height="100">
+        </a>
+    </div>
+    <div class="preview">
+        <a href="#">
+            <img src="/images/screenshot-2012GolfClassic.png" width="200" height="100">
         </a>
     </div>
     <div class="preview">
@@ -13,16 +23,6 @@
     <div class="preview">
         <a href="#">
             <img src="/images/screenshot-verilogue.png" width="200" height="100">
-        </a>
-    </div>
-    <div class="preview">
-        <a href="#">
-            <img src="http://placehold.it/200x100">
-        </a>
-    </div>
-    <div class="preview">
-        <a href="#">
-            <img src="http://placehold.it/200x100">
         </a>
     </div>
     <div class="preview">
