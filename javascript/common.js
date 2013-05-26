40b0d576702656c6b43ff123bbb9eab8687874be
cwoodford <chase@chasewoodford.com>
2013-05-25 21:19:47 -0400

    fixing js

--- javascript/common.js
+++ javascript/common.js
@@ -1,17 +1,3 @@
-// Chirp
-Chirp({
-    user: 'chase1263070', //Twitter username
-    max: 1, //Maximum number of tweets to show
-    count: 1, //Total tweets to retrieve
-    retweets: false, //Show/Don't show retweets
-    replies: false,  //Show/Don't show replies
-    cacheExpire: 1000 * 60 * 2, //Number of milliseconds to cache tweets
-    target: 'tweet-container', //Where the tweet gets loaded
-    templates: {
-        tweet: '<div class="quote-before icon-quote-right-alt"></div><div class="tweet"><p>{{html}}</p></div><div class="quote-after icon-quote-left-alt"></div><div class="left attribute twitter-attribute">{{time_ago}}&nbsp;via&nbsp;{{source}}</div><div class="right twitter-button"><a href="https://twitter.com/chase1263070" class="twitter-follow-button attribute" data-show-count="false" data-lang="en">Follow @chase1263070</a></div>'
-    }
-})
-
 // add class to header after scroll to add dropshadow effect
 $(window).scroll(function () {
     if ($(window).scrollTop() > 30) {
