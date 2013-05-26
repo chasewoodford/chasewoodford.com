232039e6c54cc930dfbc2c423f89738bf4dfeee8
cwoodford <chase@chasewoodford.com>
2013-05-25 21:20:46 -0400

    fixing js

--- blog/wp-content/themes/v2/footer.php
+++ blog/wp-content/themes/v2/footer.php
@@ -12,6 +12,7 @@
 </body>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/javascript/jquery-2.0.0.min.js"><\/script>')</script>
+<script src="/javascript/common.js" type="text/javascript"></script>
 <script src="/javascript/chirp.min.js"></script>
 <script>
     Chirp({
@@ -27,7 +28,6 @@
         }
     })
 </script>
-<script src="/javascript/common.js" type="text/javascript"></script>
 <script src="/javascript/prism.js"></script>
 <script src="/javascript/contact_form.js"></script>
 </html>
