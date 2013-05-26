2bf22b8dce4a5dbb81c49598c89a31f8f18464a4
cwoodford <chase@chasewoodford.com>
2013-05-25 21:23:25 -0400

    js test

--- javascript/common.js
+++ javascript/common.js
@@ -5,3 +5,16 @@ $(window).scroll(function () {
         $("#header").removeClass("scroll");
     }
 });
+
+Chirp({
+    user: 'chase1263070', //Twitter username
+    max: 1, //Maximum number of tweets to show
+    count: 1, //Total tweets to retrieve
+    retweets: false, //Show/Don't show retweets
+    replies: false,  //Show/Don't show replies
+    cacheExpire: 1000 * 60 * 2, //Number of milliseconds to cache tweets
+    target: 'tweet-container', //Where the tweet gets loaded
+    templates: {
+        tweet: '<div class="quote-before icon-quote-right-alt"></div><div class="tweet"><p>{{html}}</p></div><div class="quote-after icon-quote-left-alt"></div><div class="left attribute twitter-attribute">{{time_ago}}&nbsp;via&nbsp;{{source}}</div><div class="right twitter-button"><a href="https://twitter.com/chase1263070" class="twitter-follow-button attribute" data-show-count="false" data-lang="en">Follow @chase1263070</a></div>'
+    }
+});
