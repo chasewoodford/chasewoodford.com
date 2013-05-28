61e4b3129645d1170ddd82810dfa6a4fad415097
cwoodford <chase@chasewoodford.com>
2013-05-28 08:54:00 -0400

    fixing broken links, maybe

--- work/portfolio-previews.php
+++ work/portfolio-previews.php
@@ -1,22 +1,22 @@
 <section class="grid-12 text-center">
     <p class="lead"></p>
     <div class="preview">
-        <a href="./digital/dara-warren.php" data-pubdate="201304">
+        <a href="./../dara-warren.php" data-pubdate="201304">
             <img src="/images/screenshot-daraWarrent.png" title="darawarren.com" width="200" height="100">
         </a>
     </div>
     <div class="preview" data-pubdate="201210">
-        <a href="./digital/2012-golf-classic.php">
+        <a href="./../2012-golf-classic.php">
             <img src="/images/screenshot-2012GolfClassic.png" title="Verilogue 2012 Golf Classic website" width="200" height="100">
         </a>
     </div>
     <div class="preview">
-        <a href="./branding/philly-sport-past.php" data-pubdate="201207">
+        <a href="./../philly-sport-past.php" data-pubdate="201207">
             <img src="/images/screenshot-phillySportPast.png" title="PhillySportPast logo" width="200" height="100">
         </a>
     </div>
     <div class="preview">
-        <a href="./digital/verilogue-patient-journey.php" data-pubdate="201203">
+        <a href="./../verilogue-patient-journey.php" data-pubdate="201203">
             <img src="/images/screenshot-patientJourney.png" title="Verilogue Patient Journey iPad app" width="200" height="100">
         </a>
     </div>
@@ -26,12 +26,12 @@
         </a>
     </div>
     <div class="preview">
-        <a href="./digital/verilogue-point-of-practice-newsletter-archive.php" data-pubdate="201012">
+        <a href="./../verilogue-point-of-practice-newsletter-archive.php" data-pubdate="201012">
             <img src="/images/screenshot-popArchive.png" title="Verilogue Point-of-Practice newsletter archive" width="200" height="100">
         </a>
     </div>
     <div class="preview">
-        <a href="./multimedia/verilogue-2010-holiday-card.php" data-pubdate="201011">
+        <a href="./../verilogue-2010-holiday-card.php" data-pubdate="201011">
             <img src="/images/screenshot-2010HolidayCard.png" title="Verilogue 2010 holiday card" width="200" height="100">
         </a>
     </div>
