2942243b99b9bdc9193123841e0bcfd9e1f96284
cwoodford <chase@chasewoodford.com>
2013-06-04 10:48:07 -0400

    adding content to darawarren

--- work/digital/dara-warren.php
+++ work/digital/dara-warren.php
@@ -68,7 +68,9 @@
                     <p class="lead">
                         { overview }
                     </p>
-                    <p></p>
+                    <p>
+                        Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
+                    </p>
                     <br/>
                     <p class="lead">
                         { GoDaddy website builder }
