acd88db557f0056ef67e6b7ce407fee03ec7ac15
cwoodford <chase@chasewoodford.com>
2013-06-06 22:02:43 -0400

    found issue: updating jQuery from 1.7.0 to 2.0.0 breaks ajaxComplete on form

--- blog/wp-content/themes/v2/footer.php
+++ blog/wp-content/themes/v2/footer.php
@@ -10,7 +10,8 @@
     </div>
 </footer>
 </body>
-<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>-->
+<!-- WARNING: updating jQuery will break form unless you man up and fix the form, too -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/javascript/jquery-1.7.min.js"><\/script>')</script>
 <script src="/javascript/common.js"></script>
 <script src="/javascript/prism.js"></script>
