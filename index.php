b08782a1a18e2da9452161b72f678fd864d7be3a
cwoodford <chase@chasewoodford.com>
2013-06-06 20:58:29 -0400

    adjusting js loading

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
     <link rel="icon" href="/favicon.ico">
 
     <link rel="stylesheet" href="/css/compiled/style.css">
-
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </head>
 <body id="top" class="home">
 <div class="wrapper">
@@ -43,4 +41,18 @@
         <article id="tweet-container" class="grid-10 well tweets"></article>
     </div>
 </div>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<script src="/javascript/chirp.min.js"></script>
+<script>Chirp({
+        user: 'chase1263070', //Twitter username
+        max: 1, //Maximum number of tweets to show
+        count: 1, //Total tweets to retrieve
+        retweets: false, //Show/Don't show retweets
+        replies: false,  //Show/Don't show replies
+        cacheExpire: 1000 * 60 * 2, //Number of milliseconds to cache tweets
+        target: 'tweet-container', //Where the tweet gets loaded
+        templates: {
+            tweet: '<div class="quote-before icon-quote-right-alt"></div><div class="tweet"><p>{{html}}</p></div><div class="quote-after icon-quote-left-alt"></div><div class="left attribute twitter-attribute">{{time_ago}}&nbsp;via&nbsp;{{source}}</div><div class="right twitter-button"><a href="https://twitter.com/chase1263070" class="twitter-follow-button attribute" data-show-count="false" data-lang="en">Follow @chase1263070</a></div>'
+        }
+    })</script>
 <?php include ('./blog/wp-content/themes/v2/footer.php') ?>
