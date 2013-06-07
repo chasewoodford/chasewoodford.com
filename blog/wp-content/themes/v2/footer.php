f9d274f2b8a1766e8eb61ec4a52f677a6498a98f
cwoodford <chase@chasewoodford.com>
2013-06-06 21:42:17 -0400

    trying to fix form

--- blog/wp-content/themes/v2/footer.php
+++ blog/wp-content/themes/v2/footer.php
@@ -10,8 +10,8 @@
     </div>
 </footer>
 </body>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/javascript/jquery-2.0.0.min.js"><\/script>')</script>
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>-->
+<script>window.jQuery || document.write('<script src="/javascript/jquery-1.7.min.js"><\/script>')</script>
 <script src="/javascript/common.js"></script>
 <script src="/javascript/prism.js"></script>
 <script src="/javascript/contact_form.js"></script>
