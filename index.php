e7c8c3544c6f923876bb37eb13a01980d9714594
cwoodford <chase@chasewoodford.com>
2013-06-12 21:08:22 -0400

    trying to get twitter working again

--- index.php
+++ index.php
@@ -38,38 +38,40 @@
         <?php
         endwhile;
         ?>
-        <article id="tweet-container" class="grid-10 well"></article>
+        <article id="tweet-container" class="grid-10 well">
+            <?php
+
+            function buildBaseString($baseURI, $method, $params) { $r = array(); ksort($params); foreach($params as $key=>$value){ $r[] = "$key=" . rawurlencode($value); } return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r)); }
+
+
+            function buildAuthorizationHeader($oauth) { $r = 'Authorization: OAuth '; $values = array(); foreach($oauth as $key=>$value) $values[] = "$key=\"" . rawurlencode($value) . "\""; $r .= implode(', ', $values); return $r; }
+
+
+            $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+
+            $oauth_access_token = "55896667-IepSTZNxe7bQxOXiWlOyX8CMqxcuzdxKhqT6rV9Ja"; $oauth_access_token_secret = "dnDCllq5Jrd33PU5ooHKhk3p8BlgrSdaLfXBtP2mvU"; $consumer_key = "TZvnkm4qbVQhYWPlbpxrUg"; $consumer_secret = "lOECUzwETLSUxDYHBbGIr9uP58qcAFnxmWOXBeC0";
+
+
+            $oauth = array( 'oauth_consumer_key' => $consumer_key, 'oauth_nonce' => time(), 'oauth_signature_method' => 'HMAC-SHA1', 'oauth_token' => $oauth_access_token, 'oauth_timestamp' => time(), 'oauth_version' => '1.0');
+
+
+            $base_info = buildBaseString($url, 'GET', $oauth); $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret); $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true)); $oauth['oauth_signature'] = $oauth_signature;
+
+
+            // Make Requests $header = array(buildAuthorizationHeader($oauth), 'Expect:'); $options = array( CURLOPT_HTTPHEADER => $header, //CURLOPT_POSTFIELDS => $postfields, CURLOPT_HEADER => false, CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true, CURLOPT_SSL_VERIFYPEER => false);
+
+
+            $feed = curl_init(); curl_setopt_array($feed, $options); $json = curl_exec($feed); curl_close($feed);
+
+
+            $twitter_data = json_decode($json);
+
+            print_r($twitter_data);
+
+            ?>
+
+        </article>
     </div>
 </div>
 <?php include ('./blog/wp-content/themes/v2/footer.php') ?>
-<?php
-
-function buildBaseString($baseURI, $method, $params) { $r = array(); ksort($params); foreach($params as $key=>$value){ $r[] = "$key=" . rawurlencode($value); } return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r)); }
-
-
-function buildAuthorizationHeader($oauth) { $r = 'Authorization: OAuth '; $values = array(); foreach($oauth as $key=>$value) $values[] = "$key=\"" . rawurlencode($value) . "\""; $r .= implode(', ', $values); return $r; }
-
-
-$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-
-
-$oauth_access_token = "55896667-IepSTZNxe7bQxOXiWlOyX8CMqxcuzdxKhqT6rV9Ja"; $oauth_access_token_secret = "dnDCllq5Jrd33PU5ooHKhk3p8BlgrSdaLfXBtP2mvU"; $consumer_key = "TZvnkm4qbVQhYWPlbpxrUg"; $consumer_secret = "lOECUzwETLSUxDYHBbGIr9uP58qcAFnxmWOXBeC0";
-
-
-$oauth = array( 'oauth_consumer_key' => $consumer_key, 'oauth_nonce' => time(), 'oauth_signature_method' => 'HMAC-SHA1', 'oauth_token' => $oauth_access_token, 'oauth_timestamp' => time(), 'oauth_version' => '1.0');
-
-
-$base_info = buildBaseString($url, 'GET', $oauth); $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret); $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true)); $oauth['oauth_signature'] = $oauth_signature;
-
-
-// Make Requests $header = array(buildAuthorizationHeader($oauth), 'Expect:'); $options = array( CURLOPT_HTTPHEADER => $header, //CURLOPT_POSTFIELDS => $postfields, CURLOPT_HEADER => false, CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true, CURLOPT_SSL_VERIFYPEER => false);
-
-
-$feed = curl_init(); curl_setopt_array($feed, $options); $json = curl_exec($feed); curl_close($feed);
-
-
-$twitter_data = json_decode($json);
-
-print_r($twitter_data);
-
-?>
