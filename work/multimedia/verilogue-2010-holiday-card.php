2523b1ec6df215417d891beae5142fd5c29f8f47
cwoodford <chase@chasewoodford.com>
2013-08-25 17:17:55 -0400

    pushing updates to work pages

--- work/multimedia/verilogue-2010-holiday-card.php
+++ work/multimedia/verilogue-2010-holiday-card.php
@@ -66,8 +66,35 @@
                     <p class="lead">
                         { overview }
                     </p>
-                    <p></p>
-                    <p></p>
+                    <p>
+                        At the end of each year at Verilogue it has become tradition for us to send out a digital "holiday card". We send this piece to everyone we work with&mdash;physicians and patients that participate in our research, all of our clients, vendors, friends and family members. Its purpose is to summarize the accomplishments of the year and to get everyone excited for a new year full of new challenges and greater achievements. Often the card's theme is less holiday-specific and more of a thank you to everyone for helping us in our mission of enhancing disease understanding, communication and treatment dynamics.
+                    </p>
+                    <p>
+                        The 2010 holiday card is one of my favorite year-end pieces. It features a blend of real in-office dialogue from physician-patient interactions and clips from real patient-submitted video diaries. The patient video diary footage was particularly interesting to work with, not just because this was the first year we collected it, but because each diary is intense and powerful. Hearing patients describe their experience with their disease in such an honest and personal way is incredibly moving.
+                    </p>
+                    <br/>
+                    <p class="lead">
+                        { the mix }
+                    </p>
+                    <p>
+                        The file was originally hosted on the Verilogue domain as a Flash video file. Due to the varying quality levels of both the audio and video sources, in combination with bandwidth considerations, we were less worried about producing a high-quality video and more concerned about making one with a consistent quality level. That was the biggest challenge. Mixing all the audio and video down to a level that was consistent from one clip to the next.
+                    </p>
+                    <p>
+                        The audio was mixed as 5.1 surround sound, which allowed for depth between the background music and the dialogue clips in the foreground, as well as room for each dialogue clip to occupy its own unique space. Using a six-channel mix also made it easier to mask some of the disparity of the audio clips.
+                    </p>
+                    <p>
+                        The video footage was, for the most part, unaltered. The only adjustments we made were to the playback speed and cropping. The still images were taken from video clips and treated with slight zooming and panning.
+                    </p>
+                    <p>
+                        The entire video was assembled using Sony's Vegas Pro, including all video and audio editing, mixing and special effect treatments.
+                    </p>
+                    <br/>
+                    <p class="lead">
+                        { finding the clips }
+                    </p>
+                    <p>
+                        Believe it or not, the easiest part of this project was mixing the clips down. The difficult part was finding all the clips to begin with. Verilogue's database consists of over 75,000 recorded physician-patient interactions and, even though it was our first year collecting patient video diaries, we had dozens of hours of footage at our disposal. I was tasked with searching all of this data for two and three-second clips of audio and video that would weave the story we were trying to tell. Every time I watch this video I'm reminded of all the time that was invested into its creation, which is another reason in addition to the strength of the video's content that I like this piece so much.
+                    </p>
                     <br/>
                     <p>
                         To learn more about how this video was designed and developed, <a href="/contact">contact me</a>.
