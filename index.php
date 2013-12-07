8cbd3fe431ee51eaa2d3f7a087538eeb8c888487
cwoodford <chase@chasewoodford.com>
2013-12-06 19:36:02 -0500

    adding back tweet display

--- index.php
+++ index.php
@@ -40,15 +40,15 @@
         ?>
         <article id="tweet-container" class="grid-8 well" style="padding:20px;text-align:center;">
             <div class="quote-before icon-quote-right-alt"></div>
-            <div class="quote">
-        <a class="twitter-timeline"
-           data-tweet-limit="1"
-           data-link-color="#0088CC"
-           data-chrome="noborders noheader nofooter"
-           href="https://twitter.com/chase1263070"  data-widget-id="409109781861785600">Tweets by @chase1263070</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            <div class="tweet">
+                <a class="twitter-timeline"
+                   data-tweet-limit="1"
+                   data-link-color="#0088CC"
+                   data-chrome="noborders noheader nofooter"
+                   href="https://twitter.com/chase1263070"  data-widget-id="409109781861785600">Tweets by @chase1263070</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
-            <div class="quote-after icon-quote-left-alt">
+            <div class="quote-after icon-quote-left-alt"></div>
         </article>
 
     </div>
