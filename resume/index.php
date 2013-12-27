d442c52776c66caa91141c04d7cef9bf34b7a40d
cwoodford <chase@chasewoodford.com>
2013-12-27 18:39:46 -0500

    cleaning up lost class names

--- resume/index.php
+++ resume/index.php
@@ -25,7 +25,7 @@
                 <header>
                     <h3>
                         <span class="h3 blog-category">
-                            designer/developer&nbsp;&gt;
+                            designer/developer&nbsp;>
                         </span>
                         <a class="title-bar" href="#">
                             <span class="h2">chase woodford</span>
