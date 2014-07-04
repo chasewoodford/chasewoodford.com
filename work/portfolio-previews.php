eac7a57cc53e7d34f95c2496070f1254d6c0c621
cwoodford <chase@chasewoodford.com>
2014-07-03 20:02:37 -0400

    wow.js test

--- work/portfolio-previews.php
+++ work/portfolio-previews.php
@@ -1,36 +1,36 @@
 <section class="grid-12 text-center">
     <p class="lead"></p>
-    <div class="preview wow fadeInUp">
+    <div class="preview wow fadeInUp" data-wow-delay="0.2s">
         <a href="/work/digital/gary-ames.php" data-pubdate="201304">
             <img src="/images/screenshot-garyAmes.gif" title="garyames.net" width="200" height="100">
         </a>
     </div>
-    <div class="preview wow fadeInUp" data-wow-delay="0.1s">
+    <div class="preview wow fadeInUp" data-wow-delay="0.4s">
         <a href="/work/digital/revealmr.php" data-pubdate="201304">
             <img src="/images/screenshot-revealmr.gif" title="RevealMR" width="200" height="100">
         </a>
     </div>
-    <div class="preview wow fadeInUp" data-wow-delay="0.2s">
+    <div class="preview wow fadeInUp" data-wow-delay="0.6s">
         <a href="/work/digital/dara-warren.php" data-pubdate="201304">
             <img src="/images/screenshot-daraWarren.gif" title="darawarren.com" width="200" height="100">
         </a>
     </div>
-    <div class="preview wow fadeInUp" data-wow-delay="0.3s">
+    <div class="preview wow fadeInUp" data-wow-delay="0.8s">
         <a href="/work/digital/2012-golf-classic.php" data-pubdate="201210">
             <img src="/images/screenshot-2012GolfClassic.gif" title="Verilogue 2012 Golf Classic website" width="200" height="100">
         </a>
     </div>
-    <div class="preview wow fadeInUp" data-wow-delay="0.1s">
+    <div class="preview wow fadeInUp" data-wow-delay="0.2s">
         <a href="/work/branding/philly-sport-past.php" data-pubdate="201207">
             <img src="/images/screenshot-phillySportPast.png" title="PhillySportPast logo" width="200" height="100">
         </a>
     </div>
-    <div class="preview wow fadeInUp" data-wow-delay="0.2s">
+    <div class="preview wow fadeInUp" data-wow-delay="0.4s">
         <a href="/work/multimedia/carecoach-eliterati.php" data-pubdate="201110">
             <img src="/images/screenshot-eliterati.png" title="eLiterati" width="200" height="100">
         </a>
     </div>
-    <div class="preview wow fadeInUp" data-wow-delay="0.3s">
+    <div class="preview wow fadeInUp" data-wow-delay="0.6s">
         <a href="/work/multimedia/verilogue-2010-holiday-card.php" data-pubdate="201011">
             <img src="/images/screenshot-2010HolidayCard.png" title="Verilogue 2010 holiday card" width="200" height="100">
         </a>
