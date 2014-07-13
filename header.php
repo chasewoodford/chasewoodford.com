f57060d22cde6d15a16428d408773b67ac80227c
cwoodford <chase@chasewoodford.com>
2014-07-12 21:26:53 -0400

    trying to get sitemap generator to work better

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
     <div class="grid-8 header-inner">
         <div class="links" role="navigation">
             <a class="header-link" href="/" title="home" tabindex="6">home</a><span class="bullet">&bull;</span>
-            <a class="header-link" href="/resume" tabindex="7">resume</a><span class="bullet">&bull;</span>
-            <a class="header-link" href="/contact" title="contact" tabindex="8">contact</a>
+            <a class="header-link" href="/resume/index.php" tabindex="7">resume</a><span class="bullet">&bull;</span>
+            <a class="header-link" href="/contact/index.php" title="contact" tabindex="8">contact</a>
         </div>
         <!--<img class="logo pulse" src="/images/logo.png" alt="Chase Woodford &#124; Designer &amp; Developer"/>-->
         <aside class="right search-box" role="search">
@@ -17,9 +17,9 @@
             </form>
         </aside>
         <nav class="nav-primary" role="navigation">
-            <a class="nav-link no-decoration about" href="/about" title="about" tabindex="1"><span class="h2">about</span></a>
-            <a class="nav-link no-decoration work" href="/work" title="work" tabindex="2"><span class="h2">work</span></a>
-            <a class="nav-link no-decoration blog" href="/blog" title="blog" tabindex="3"><span class="h2">blog</span></a>
+            <a class="nav-link no-decoration about" href="/about/index.php" title="about" tabindex="1"><span class="h2">about</span></a>
+            <a class="nav-link no-decoration work" href="/work/index.php" title="work" tabindex="2"><span class="h2">work</span></a>
+            <a class="nav-link no-decoration blog" href="/blog/index.php" title="blog" tabindex="3"><span class="h2">blog</span></a>
         </nav>
     </div>
 </div>
