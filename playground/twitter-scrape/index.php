09a7ec398061dcbb0b5023967d06e7d657a8f076
cwoodford <chase@chasewoodford.com>
2014-08-23 15:21:25 -0400

    another test

--- playground/twitter-scrape/index.php
+++ playground/twitter-scrape/index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Twitter</title>
-</head>
+<html>
 <body>
-<?php
-  require('twitter.class.php');
-  $twitter = new twitter_class();
-  echo $twitter->getTweets('search term', 10);
-?>
+<?php include "twitter.php"; ?>
 </body>
 </html>
