545cb7b8427c9213691c4f89b57d7433f74c49e2
cwoodford <chase@chasewoodford.com>
2014-08-29 19:36:13 -0400

    JSON echo test

--- playground/weight-tracker/index.php
+++ playground/weight-tracker/index.php
@@ -83,7 +83,7 @@ $selected = mysql_select_db("db541682804",$dbhandle)
 or die("Could not select examples");
 
 //execute the SQL query and return records
-$result = mysql_query("SELECT id, date, weight FROM weight_tracker");
+$result = mysql_query("SELECT date, weight FROM weight_tracker");
 
 //fetch tha data from the database
 while ($row = mysql_fetch_array($result)) {
