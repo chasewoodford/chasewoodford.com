d96296ace82a3efb7d6ab4c17c390f7cb48ece7e
cwoodford <chase@chasewoodford.com>
2014-08-29 20:49:32 -0400

    echo test

--- playground/weight-tracker/index.php
+++ playground/weight-tracker/index.php
@@ -37,6 +37,7 @@ mysql_close($dbhandle);
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
         ['Date',	'Weight'],
+        <?php $entry ?>
 //                ['8/18/2010',	159.0],
 //                ['8/29/2010',	159.5],
 //                ['9/15/2010',	158.0],
