677dc3a335ae3125434fb6f262a47ab29c1d1297
cwoodford <chase@chasewoodford.com>
2014-08-29 20:52:00 -0400

    echo test

--- playground/weight-tracker/index.php
+++ playground/weight-tracker/index.php
@@ -23,7 +23,7 @@ $result = mysql_query("SELECT id, date, weight FROM weight_tracker");
 //fetch tha data from the database
 while ($row = mysql_fetch_array($result)) {
     $entry = "['".$row{'date'}."',".$row{'weight'}."],";
-    echo $entry;
+    $entryList = $entry;
 }
 //close the connection
 mysql_close($dbhandle);
@@ -37,7 +37,7 @@ mysql_close($dbhandle);
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
         ['Date',	'Weight'],
-        <?php echo $entry ?>
+        <?php echo $entryList ?>
 //                ['8/18/2010',	159.0],
 //                ['8/29/2010',	159.5],
 //                ['9/15/2010',	158.0],
