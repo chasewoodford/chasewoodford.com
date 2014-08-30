34c04f039b16d42923cc637258e5202941f674c0
cwoodford <chase@chasewoodford.com>
2014-08-29 21:01:10 -0400

    echo test

--- playground/weight-tracker/index.php
+++ playground/weight-tracker/index.php
@@ -27,7 +27,7 @@ while ($row = mysql_fetch_array($result)) {
 //close the connection
 mysql_close($dbhandle);
 ?>
-<!--<div id="chart_div" style="width: 100%; height: 500px;"></div>-->
+<div id="chart_div" style="width: 100%; height: 500px;"></div>
 
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript">
